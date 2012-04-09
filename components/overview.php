0e891d145d512b08f602beb7700fadd782657ada
John Blackbourn <johnbillion@gmail.com>
2012-04-09 20:56:40 +0100

    For some reason we're using metric ks and Ms instead of k(i)Bs and M(i)Bs. We'd better fix that. Output the memory usage in percentage, and the page gen time in percentage if it's high enough.

--- components/overview.php
+++ components/overview.php
@@ -10,20 +10,20 @@ class QM_Overview extends QM {
 	}
 
 	function admin_title( $title ) {
-		$title[] = sprintf( __( '%s<small>S</small>', 'query-monitor' ), number_format_i18n( $this->data['load_time'], 2 ) );
-		$title[] = sprintf( __( '%s<small>M</small>', 'query-monitor' ), number_format_i18n( ( $this->data['memory'] / 1000 / 1000 ), 1 ) );
+		$title[] = sprintf( __( '%s<small>S</small>', 'query-monitor' ), number_format_i18n( $this->data['time'], 2 ) );
+		$title[] = sprintf( __( '%s<small>MB</small>', 'query-monitor' ), number_format_i18n( ( $this->data['memory'] / 1024 / 1024 ), 1 ) );
 		return $title;
 	}
 
 	function output( $args, $data ) {
 
-		$http_time = null;
-		$http = $this->get_component( 'http' );
-
-		$db_query_num = null;
+		$http_time      = null;
+		$db_query_num   = null;
 		$db_query_types = array();
-		$db_query_time = array();
-		$db_queries = $this->get_component( 'db_queries' );
+		$http           = $this->get_component( 'http' );
+		$db_queries     = $this->get_component( 'db_queries' );
+		$time_usage     = '';
+		$memory_usage   = '';
 
 		if ( $http and isset( $http->data['http'] ) ) {
 			foreach ( $http->data['http'] as $row ) {
@@ -40,48 +40,43 @@ class QM_Overview extends QM {
 			$db_ltime = number_format_i18n( $db_queries->data['total_time'], 10 );
 		}
 
-		$total_stime = number_format_i18n( $data['load_time'], 4 );
-		$total_ltime = number_format_i18n( $data['load_time'], 10 );
-		$excl_stime  = number_format_i18n( $data['load_time'] - $http_time, 4 );
-		$excl_ltime  = number_format_i18n( $data['load_time'] - $http_time, 10 );
-
-		if ( empty( $http_time ) )
-			$timespan = 1;
-		else
-			$timespan = 2;
+		$total_stime = number_format_i18n( $data['time'], 4 );
+		$total_ltime = number_format_i18n( $data['time'], 10 );
+		$excl_stime  = number_format_i18n( $data['time'] - $http_time, 4 );
+		$excl_ltime  = number_format_i18n( $data['time'] - $http_time, 10 );
 
 		echo '<div class="qm" id="' . $this->id() . '">';
 		echo '<table cellspacing="0">';
 		echo '<tbody>';
 
+		$memory_usage .= '<br /><span class="qm-info">' . sprintf( __( '%1$s%% of %2$s kB', 'query-monitor' ), number_format_i18n( $data['memory_usage'], 1 ), number_format_i18n( $data['memory_limit'] / 1024 ) ) . '</span>';
+
 		echo '<tr>';
 		echo '<th>' . __( 'Peak memory usage', 'query-monitor' ) . '</th>';
-		echo '<td title="' . esc_attr( sprintf( __( '%s bytes', 'query-monitor' ), number_format_i18n( $data['memory'] ) ) ) . '">' . sprintf( __( '%s kB', 'query-monitor' ), number_format_i18n( $data['memory'] / 1000 ) ) . '</td>';
+		echo '<td title="' . esc_attr( sprintf( __( '%s bytes', 'query-monitor' ), number_format_i18n( $data['memory'] ) ) ) . '">' . sprintf( __( '%s kB', 'query-monitor' ), number_format_i18n( $data['memory'] / 1024 ) ) . $memory_usage . '</td>';
 		echo '</tr>';
+
+		if ( isset( $http_time ) )
+			$time_usage .= '<br /><span class="qm-info">' . sprintf( __( '%s without HTTP requests', 'query-monitor' ), $excl_stime ) . '</span>';
+
+		if ( $data['time_usage'] > 25 ) /* Only bother with generation time if it's above 25%: */
+			$time_usage .= '<br /><span class="qm-info">' . sprintf( __( '%1$s%% of %2$ss limit', 'query-monitor' ), number_format_i18n( $data['time_usage'], 1 ), number_format_i18n( $data['time_limit'] ) ) . '</span>';
 
 		echo '<tr>';
-		echo '<th rowspan=" ' . $timespan . '">' . __( 'Page generation time', 'query-monitor' ) . '</th>';
-		echo "<td title='{$total_ltime}'>{$total_stime}</td>";
+		echo '<th>' . __( 'Page generation time', 'query-monitor' ) . '</th>';
+		echo "<td title='{$total_ltime}'>{$total_stime}{$time_usage}</td>";
 		echo '</tr>';
 
-		if ( isset( $http_time ) ) {
-			echo '<tr>';
-			echo "<td title='{$excl_ltime}'>" . sprintf( __( '%s w/o HTTP requests', 'query-monitor' ), $excl_stime ) . "</td>";
-			echo '</tr>';
-		}
-
-		if ( !empty( $db_query_time ) ) {
+		if ( isset( $db_query_num ) ) {
 			echo '<tr>';
 			echo '<th>' . __( 'Database query time', 'query-monitor' ) . '</th>';
 			echo "<td title='{$db_ltime}'>{$db_stime}</td>";
 			echo '</tr>';
-		}
-
-		if ( isset( $db_query_num ) ) {
 			echo '<tr>';
 			echo '<th>' . __( 'Database queries', 'query-monitor' ) . '</th>';
 			echo '<td>';
 
+			# @TODO i18n
 			foreach ( $db_query_num as $type_name => $type_count )
 				$db_query_types[] = sprintf( '%1$s: %2$s', $type_name, number_format_i18n( $type_count ) );
 
@@ -99,12 +94,17 @@ class QM_Overview extends QM {
 
 	function process() {
 
+		$this->data['time']       = $this->timer_stop_float();
+		$this->data['time_limit'] = ini_get( 'max_execution_time' );
+		$this->data['time_usage'] = ( 100 / $this->data['time_limit'] ) * $this->data['time'];
+
 		if ( function_exists( 'memory_get_peak_usage' ) )
 			$this->data['memory'] = memory_get_peak_usage();
 		else
 			$this->data['memory'] = memory_get_usage();
 
-		$this->data['load_time'] = $this->timer_stop_float();
+		$this->data['memory_limit'] = $this->convert_hr_to_bytes( ini_get( 'memory_limit' ) );
+		$this->data['memory_usage'] = ( 100 / $this->data['memory_limit'] ) * $this->data['memory'];
 
 	}
 
