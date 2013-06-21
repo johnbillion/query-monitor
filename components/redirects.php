fc83ec940c837e4712edb2d6ab6e17321ef4c38b
John Blackbourn <johnbillion@gmail.com>
2013-06-21 13:24:44 +0100

    New 'Redirects' component. Adds an 'X-QM-Redirect-Trace' header whenever `wp_redirect()` performs a redirect. Allows you to debug redirects via an HTTP inspector.

---
+++ components/redirects.php
@@ -0,0 +1,37 @@
+<?php
+
+class QM_Component_Redirects extends QM_Component {
+
+	var $id = 'redirects';
+
+	function __construct() {
+		parent::__construct();
+		add_filter( 'wp_redirect', array( $this, 'filter_wp_redirect' ), 999, 2 );
+	}
+
+	public function filter_wp_redirect( $location, $status ) {
+
+		global $querymonitor;
+
+		if ( !$location or !$querymonitor->show_query_monitor() )
+			return $location;
+
+		$trace = QM_Util::backtrace();
+		unset( $trace[0] ); # wp_redirect filter
+
+		header( sprintf( 'X-QM-Redirect-Trace: %s',
+			implode( ', ', $trace )
+		) );
+
+		return $location;
+
+	}
+
+}
+
+function register_qm_redirects( array $qm ) {
+	$qm['redirects'] = new QM_Component_Redirects;
+	return $qm;
+}
+
+add_filter( 'query_monitor_components', 'register_qm_redirects', 140 );
