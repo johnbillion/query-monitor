123ddcdaacb4be28df3e7dfa1639a2ed46547526
John Blackbourn <johnbillion@gmail.com>
2013-11-16 01:39:50 +0000

    Mark as 2.5

--- query-monitor.php
+++ query-monitor.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Query Monitor
 Description: Monitoring of database queries, hooks, conditionals and more.
-Version:     2.5-beta1
+Version:     2.5
 Author:      John Blackbourn
 Author URI:  https://johnblackbourn.com/
 Text Domain: query-monitor
