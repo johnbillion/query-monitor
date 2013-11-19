12b43973491e9f2d7972798e3f99adc2f9f48278
John Blackbourn <johnbillion@gmail.com>
2013-11-19 21:27:30 +0000

    Update the headers so I don't waste time trying to find out what is wrong with the bloody copyright symbol

--- Plugin.php
+++ Plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Copyright © 2013 John Blackbourn
+Copyright 2013 John Blackbourn
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- wp-content/db.php
+++ wp-content/db.php
@@ -9,7 +9,7 @@ additional database query information in Query Monitor's output.
 
 *********************************************************************
 
-© 2013 John Blackbourn
+Copyright 2013 John Blackbourn
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
