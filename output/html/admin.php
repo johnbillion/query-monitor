1b66ae2c03de0e5fe3a1431a7652e0dd01d2ccc1
John Blackbourn <johnbillion@gmail.com>
2013-12-04 01:27:02 +0000

    Restructure the admin component

---
+++ output/html/admin.php
@@ -0,0 +1,131 @@
+<?php
+/*
+
+© 2013 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+class QM_Output_Html_Admin extends QM_Output_Html {
+
+	public function output() {
+
+		$data = $this->component->get_data();
+
+		if ( empty( $data ) )
+			return;
+
+		echo '<div class="qm qm-half" id="' . $this->component->id() . '">';
+		echo '<table cellspacing="0">';
+		echo '<thead>';
+		echo '<tr>';
+		echo '<th colspan="3">' . $this->component->name() . '</th>';
+		echo '</tr>';
+		echo '</thead>';
+		echo '<tbody>';
+
+		echo '<tr>';
+		echo '<td class="qm-ltr">get_current_screen()</td>';
+		echo '<td>';
+
+		if ( is_object( $data['admin'] ) ) {
+			echo '<table class="qm-inner" cellspacing="0">';
+			echo '<tbody>';
+			foreach ( $data['admin'] as $key => $value ) {
+				echo '<tr>';
+				echo "<td class='qm-var'>{$key}:</td>";
+				echo '<td>';
+				echo $value;
+				if ( !empty( $value ) and ( $data['current_screen']->$key != $value ) )
+					echo '&nbsp;(<a href="http://core.trac.wordpress.org/ticket/14886" class="qm-warn" title="' . esc_attr__( 'This value may not be as expected. Please see WordPress bug #14886.', 'query-monitor' ) . '" target="_blank">!</a>)';
+				echo '</td>';
+				echo '</tr>';
+			}
+			echo '</tbody>';
+			echo '</table>';
+		} else {
+			echo $data['admin'];
+		}
+
+		echo '</td>';
+		echo '</tr>';
+
+		echo '<tr>';
+		echo '<td class="qm-ltr">$pagenow</td>';
+		echo "<td>{$data['pagenow']}</td>";
+		echo '</tr>';
+
+		$screens = array(
+			'edit'            => true,
+			'edit-comments'   => true,
+			'edit-tags'       => true,
+			'link-manager'    => true,
+			'plugins'         => true,
+			'plugins-network' => true,
+			'sites-network'   => true,
+			'themes-network'  => true,
+			'upload'          => true,
+			'users'           => true,
+			'users-network'   => true,
+		);
+
+		if ( !empty( $data['current_screen'] ) and isset( $screens[$data['current_screen']->base] ) ) {
+
+			# And now, WordPress' legendary inconsistency comes into play:
+
+			if ( !empty( $data['current_screen']->taxonomy ) )
+				$col = $data['current_screen']->taxonomy;
+			else if ( !empty( $data['current_screen']->post_type ) )
+				$col = $data['current_screen']->post_type . '_posts';
+			else
+				$col = $data['current_screen']->base;
+
+			if ( !empty( $data['current_screen']->post_type ) and empty( $data['current_screen']->taxonomy ) )
+				$cols = $data['current_screen']->post_type . '_posts';
+			else
+				$cols = $data['current_screen']->id;
+
+			if ( 'edit-comments' == $col )
+				$col = 'comments';
+			else if ( 'upload' == $col )
+				$col = 'media';
+			else if ( 'link-manager' == $col )
+				$col = 'link';
+
+			echo '<tr>';
+			echo '<td rowspan="2">' . __( 'Column Filters', 'query-monitor' ) . '</td>';
+			echo "<td colspan='2'>manage_<span class='qm-current'>{$cols}</span>_columns</td>";
+			echo '</tr>';
+			echo '<tr>';
+			echo "<td colspan='2'>manage_<span class='qm-current'>{$data['current_screen']->id}</span>_sortable_columns</td>";
+			echo '</tr>';
+
+			echo '<tr>';
+			echo '<td rowspan="1">' . __( 'Column Action', 'query-monitor' ) . '</td>';
+			echo "<td colspan='2'>manage_<span class='qm-current'>{$col}</span>_custom_column</td>";
+			echo '</tr>';
+
+		}
+
+		echo '</tbody>';
+		echo '</table>';
+		echo '</div>';
+
+	}
+
+}
+
+function register_qm_admin_output_html( QM_Output $output = null, QM_Component $component ) {
+	return new QM_Output_Html_Admin( $component );
+}
+
+add_filter( 'query_monitor_output_html_admin', 'register_qm_admin_output_html', 10, 2 );
