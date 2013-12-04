2ddbab870c121c8762e783048749797630101abb
John Blackbourn <johnbillion@gmail.com>
2013-12-04 00:13:52 +0000

    Header output base class

---
+++ Headers.php
@@ -0,0 +1,31 @@
+<?php
+/*
+
+© 2013 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+class QM_Output_Headers implements QM_Output {
+
+	public function __construct() {}
+
+	public function output( QM_Component $component ) {
+		# Headers output does nothing by default
+		return false;
+	}
+
+	final public function get_type() {
+		return 'headers';
+	}
+
+}
