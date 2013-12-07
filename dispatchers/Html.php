91296786e11f7a59024d2c41a49fe3dcb9ee182d
John Blackbourn <johnbillion@gmail.com>
2013-12-07 21:18:12 +0000

    Replace some copyright symbols that have snuck in again. Saves us worrying about encoding issues.

--- dispatchers/Html.php
+++ dispatchers/Html.php
@@ -1,7 +1,7 @@
 <?php
 /*
 
-© 2013 John Blackbourn
+Copyright 2013 John Blackbourn
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
