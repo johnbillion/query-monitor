79e1955869225b1a02b87a8fabdd633903e35224
John Blackbourn <johnbillion@gmail.com>
2014-02-02 04:57:22 +0000

    Remove a now-redundant condition in `should_process()`

--- query-monitor.php
+++ query-monitor.php
@@ -144,13 +144,7 @@ class QueryMonitor extends QM_Plugin {
 
 		# Don't process if the minimum required actions haven't fired:
 
-		if ( QM_Util::is_ajax() ) {
-
-			if ( ! did_action( 'admin_init' ) ) {
-				return false;
-			}
-
-		} else if ( is_admin() ) {
+		if ( is_admin() ) {
 
 			if ( ! did_action( 'admin_init' ) ) {
 				return false;
