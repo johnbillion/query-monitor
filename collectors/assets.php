7e1044f36075260192e969b7f041ac0755313c68
John Blackbourn <johnbillion@gmail.com>
2014-10-07 21:01:04 +0100

    First pass at Scripts & Styles output.

---
+++ collectors/assets.php
@@ -0,0 +1,58 @@
+<?php
+/*
+Copyright 2014 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+class QM_Collector_Assets extends QM_Collector {
+
+	public $id = 'assets';
+
+	public function __construct() {
+		parent::__construct();
+		add_action( 'admin_print_footer_scripts', array( $this, 'action_print_footer_scripts' ) );
+		add_action( 'wp_print_footer_scripts',    array( $this, 'action_print_footer_scripts' ) );
+	}
+
+	public function action_print_footer_scripts() {
+		global $wp_scripts, $wp_styles;
+
+		$this->data['raw_scripts'] = $wp_scripts;
+		$this->data['raw_styles']  = $wp_styles;
+
+		$this->data['header_scripts'] = array_diff( $wp_scripts->done, $wp_scripts->in_footer );
+		$this->data['footer_scripts'] = $wp_scripts->in_footer;
+		$this->data['header_styles']  = $wp_styles->done;
+
+	}
+
+	public function name() {
+		return __( 'Scripts & Styles', 'query-monitor' );
+	}
+
+	public function process() {
+
+		global $wp_scripts, $wp_styles;
+
+		// @TODO remove
+
+	}
+
+}
+
+function register_qm_collector_assets( array $qm ) {
+	$qm['assets'] = new QM_Collector_Assets;
+	return $qm;
+}
+
+add_filter( 'query_monitor_collectors', 'register_qm_collector_assets', 80 );
---
+++ output/html/assets.php
@@ -0,0 +1,156 @@
+<?php
+/*
+
+Copyright 2014 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+class QM_Output_Html_Assets extends QM_Output_Html {
+
+	public $id = 'hooks';
+
+	public function __construct( QM_Collector $collector ) {
+		parent::__construct( $collector );
+		add_filter( 'query_monitor_menus', array( $this, 'admin_menu' ), 70 );
+	}
+
+	public function output() {
+
+		$data = $this->collector->get_data();
+
+		if ( empty( $data ) ) {
+			return;
+		}
+
+		echo '<div class="qm" id="' . esc_attr( $this->collector->id() ) . '">';
+		echo '<table cellspacing="0">';
+		echo '<thead>';
+		echo '<tr>';
+		echo '<th colspan="2">' . esc_html( $this->collector->name() ) . '</th>';
+		echo '<th>' . __( 'Dependencies', 'query-monitor' ) . '</th>';
+	//	echo '<th>' . __( 'Component', 'query-monitor' ) . '</th>';
+		echo '<th>' . __( 'Version', 'query-monitor' ) . '</th>';
+		echo '</tr>';
+		echo '</thead>';
+		echo '<tbody>';
+
+		// @TODO concat, do_concat, concat_version
+
+		$rowspan = count( $data['header_scripts'] );
+		$first   = true;
+
+		echo '<tr>';
+		echo "<td valign='top' rowspan='{$rowspan}'>" . __( 'Header&nbsp;Scripts', 'query-monitor' ) . "</td>";	
+
+		foreach ( $data['header_scripts'] as $handle ) {
+			if ( !$first ) {
+				echo '<tr>';
+			}
+
+			$this->dependency_row( $data['raw_scripts']->registered[$handle], $data );
+
+			echo '</tr>';
+			$first = false;
+		}
+
+		$rowspan = count( $data['footer_scripts'] );
+		$first   = true;
+
+		echo '<tr>';
+		echo "<td valign='top' rowspan='{$rowspan}'>" . __( 'Footer&nbsp;Scripts', 'query-monitor' ) . "</td>";	
+
+		foreach ( $data['footer_scripts'] as $handle ) {
+			if ( !$first ) {
+				echo '<tr>';
+			}
+
+			$this->dependency_row( $data['raw_scripts']->registered[$handle], $data );
+
+			echo '</tr>';
+			$first = false;
+		}
+
+		$rowspan = count( $data['header_styles'] );
+		$first   = true;
+
+		echo '<tr>';
+		echo "<td valign='top' rowspan='{$rowspan}'>" . __( 'Styles', 'query-monitor' ) . "</td>";	
+
+		foreach ( $data['header_styles'] as $handle ) {
+			if ( !$first ) {
+				echo '<tr>';
+			}
+
+			$this->dependency_row( $data['raw_styles']->registered[$handle], $data );
+
+			echo '</tr>';
+			$first = false;
+		}
+
+		echo '</tbody>';
+		echo '</table>';
+		echo '</div>';
+
+	}
+
+	protected function dependency_row( _WP_Dependency $script, array $data ) {
+
+	//	$path = $script->src;
+
+	//	if ( preg_match( '#^//#', $path ) ) {
+	//		$path = is_ssl() ? 'https:' . $path : 'http:' . $path;
+	//	} else if ( preg_match( '#^/#', $path ) ) {
+	//		$path = home_url( $path );
+	//	} else {
+	//		$path = set_url_scheme( $path );
+	//	}
+
+	//	$path      = str_replace( set_url_scheme( WP_PLUGIN_URL ), WP_PLUGIN_DIR, $path );
+	//	$path      = str_replace( set_url_scheme( WP_CONTENT_URL ), WP_CONTENT_DIR, $path );
+	//	$component = QM_Util::get_file_component( $path );
+
+		if ( empty( $script->ver ) ) {
+			$ver = '<em class="qm-info">' . $data['raw_scripts']->default_version . '</em>';
+		} else {
+			$ver = esc_html( $script->ver );
+		}
+
+		if ( empty( $script->src ) ) {
+			$src = '&nbsp;';
+		} else {
+			$src = $script->src;
+		}
+
+		echo '<td valign="top">' . $script->handle . '<br><span class="qm-info">' . $src . '</span></td>';
+		echo '<td valign="top">' . implode( ', ', $script->deps ) . '</td>';
+	//	echo '<td valign="top">' . $component->name . '</td>';
+		echo '<td valign="top">' . $ver . '</td>';
+
+	}
+
+	public function admin_menu( array $menu ) {
+
+		$menu[] = $this->menu( array(
+			'title' => $this->collector->name(),
+		) );
+		return $menu;
+
+	}
+
+}
+
+function register_qm_output_html_assets( QM_Output $output = null, QM_Collector $collector ) {
+	return new QM_Output_Html_Assets( $collector );
+}
+
+add_filter( 'query_monitor_output_html_assets', 'register_qm_output_html_assets', 10, 2 );
