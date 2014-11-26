c8b8b1cf42f41f151c1323c488ce97c8b8199885
Jess Froholdt Johannessen <jess.johannessen@coolgray.dk>
2014-11-26 14:31:50 +0100

    Make the menu item toggle the view

--- assets/query-monitor.js
+++ assets/query-monitor.js
@@ -102,7 +102,7 @@ jQuery( function($) {
 				}
 
 			}
-			$('#qm').show();
+			$('#qm').toggle();
 		});
 
 		$('#wp-admin-bar-query-monitor,#wp-admin-bar-query-monitor-default').show();
@@ -194,7 +194,7 @@ jQuery( function($) {
 
 /**
  * This is a modified version of:
- * 
+ *
  * jQuery table-sort v0.1.1
  * https://github.com/gajus/table-sort
  *
@@ -209,60 +209,60 @@ jQuery( function($) {
 		var settings = $.extend({
 			'debug': false
 		}, options);
-		
+
 		// @param	object	columns	NodeList table colums.
 		// @param	integer	row_width	defines the number of columns per row.
 		var table_to_array	= function (columns, row_width) {
 			if (settings.debug) {
 				console.time('table to array');
 			}
-		
+
 			columns = Array.prototype.slice.call(columns, 0);
-			
+
 			var rows      = [];
 			var row_index = 0;
-			
+
 			for (var i = 0, j = columns.length; i < j; i += row_width) {
 				var row	= [];
-				
+
 				for (var k = 0, l = row_width; k < l; k++) {
 					var e = columns[i+k];
-					
+
 					var data = e.dataset.qmSortWeight;
-					
+
 					if (data === undefined) {
 						data = e.textContent || e.innerText;
 					}
-					
+
 					var number = parseFloat(data);
-					
+
 					data = isNaN(number) ? data : number;
-					
+
 					row.push(data);
 				}
-				
+
 				rows.push({index: row_index++, data: row});
 			}
-			
+
 			if (settings.debug) {
 				console.timeEnd('table to array');
 			}
-			
+
 			return rows;
 		};
-		
+
 		if (!settings.target || !settings.target instanceof $) {
 			throw 'Target is not defined or it is not instance of jQuery.';
 		}
-		
+
 		settings.target.each(function () {
 			var table = $(this);
-			
+
 			table.find('.qm-sort').on('click', function (e) {
 				var desc = $(this).hasClass('qm-sort-desc');
-				
+
 				var index = $(this).closest('th').index();
- 
+
 				table.find('th').removeClass('qm-sorted-asc qm-sorted-desc');
 
 				if ( desc )
@@ -272,63 +272,63 @@ jQuery( function($) {
 
 				table.find('tbody:not(.qm-sort-no)').each(function () {
 					var tbody = $(this);
-					
+
 					var rows = this.rows;
-					
+
 					var anomalies = $(rows).has('[colspan]').detach();
-					
+
 					var columns = this.getElementsByTagName('td');
-					
+
 					if (this.data_matrix === undefined) {
 						this.data_matrix = table_to_array(columns, $(rows[0]).find('td').length);
 					}
-					
+
 					var data = this.data_matrix;
-					
+
 					if (settings.debug) {
 						console.time('sort data');
 					}
-					
+
 					data.sort(function (a, b) {
 						if (a.data[index] == b.data[index]) {
 							return 0;
 						}
-						
+
 						return (desc ? a.data[index] > b.data[index] : a.data[index] < b.data[index]) ? -1 : 1;
 					});
-										
+
 					if (settings.debug) {
 						console.timeEnd('sort data');
 						console.time('build table');
 					}
-					
+
 					// Will use this to re-attach the tbody object.
 					var table = tbody.parent();
-					
+
 					// Detach the tbody to prevent unnecassy overhead related
 					// to the browser environment.
 					tbody = tbody.detach();
-					
+
 					// Convert NodeList into an array.
 					rows = Array.prototype.slice.call(rows, 0);
-					
+
 					var last_row = rows[data[data.length-1].index];
-					
+
 					for (var i = 0, j = data.length-1; i < j; i++) {
 						tbody[0].insertBefore(rows[data[i].index], last_row);
-						
+
 						// Restore the index.
 						data[i].index = i;
 					}
-					
+
 					// // Restore the index.
 					data[data.length-1].index = data.length-1;
-					
+
 					tbody.prepend(anomalies);
-					
+
 					table.append(tbody);
-					
-					
+
+
 					if (settings.debug) {
 						console.timeEnd('build table');
 					}
