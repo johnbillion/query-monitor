911dd8a7388b9ed40e460537a94151d5fb18bf3f
John Blackbourn <johnbillion@gmail.com>
2015-04-11 00:40:37 +0100

    Add some missing i18n.

--- collectors/debug_bar.php
+++ collectors/debug_bar.php
@@ -25,7 +25,7 @@ final class QM_Collector_Debug_Bar extends QM_Collector {
 
 	public function name() {
 		$title = $this->get_panel()->title();
-		return sprintf( 'Debug Bar: %s', $title );
+		return sprintf( __( 'Debug Bar: %s', 'query-monitor' ), $title );
 	}
 
 	public function set_panel( Debug_Bar_Panel $panel ) {
