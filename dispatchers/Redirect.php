886eb50697fe6826cc8c63e67ad9f66f223f2845
John Blackbourn <johnbillion@gmail.com>
2015-08-05 00:39:36 +0100

    Introduce the Redirect dispatcher as a follow-up to the changes made to dispatchers in f0c2f30.

---
+++ dispatchers/Redirect.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Copyright 2009-2015 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+class QM_Dispatcher_Redirect extends QM_Dispatcher {
+
+	public $id = 'redirect';
+
+	public function __construct( QM_Plugin $qm ) {
+		parent::__construct( $qm );
+
+		add_filter( 'wp_redirect', array( $this, 'filter_wp_redirect' ), 999, 2 );
+
+	}
+
+	/**
+	 *
+	 * @param string $location The path to redirect to.
+	 * @param int    $status   Status code to use.
+	 */
+	public function filter_wp_redirect( $location, $status ) {
+
+		if ( ! $this->should_dispatch() ) {
+			return $location;
+		}
+
+		$out = $this->get_output( 'headers' );
+
+		foreach ( $out['output'] as $id => $output ) {
+			foreach ( $output as $key => $value ) {
+				header( sprintf( 'X-QM-%s: %s', $key, $value ) );
+			}
+		}
+
+		return $location;
+
+	}
+
+	protected function before_output() {
+
+		require_once $this->qm->plugin_path( 'output/Headers.php' );
+
+		foreach ( glob( $this->qm->plugin_path( 'output/headers/*.php' ) ) as $file ) {
+			include $file;
+		}
+	}
+
+	public function is_active() {
+
+		if ( ! $this->user_can_view() ) {
+			return false;
+		}
+
+		# If the headers have already been sent then we can't do anything about it
+		if ( headers_sent() ) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+}
+
+function register_qm_dispatcher_redirect( array $dispatchers, QM_Plugin $qm ) {
+	$dispatchers['redirect'] = new QM_Dispatcher_Redirect( $qm );
+	return $dispatchers;
+}
+
+add_filter( 'qm/dispatchers', 'register_qm_dispatcher_redirect', 10, 2 );
