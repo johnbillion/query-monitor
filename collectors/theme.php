31f53ad33a910cf385004adda98a5245f5f420f9
John Blackbourn <johnbillion@gmail.com>
2015-08-05 00:02:58 +0100

    Remove the use of the global `$template` variable and use the value of the `template_include` filter instead. See #129.

--- collectors/theme.php
+++ collectors/theme.php
@@ -24,7 +24,8 @@ class QM_Collector_Theme extends QM_Collector {
 
 	public function __construct() {
 		parent::__construct();
-		add_filter( 'body_class', array( $this, 'filter_body_class' ), 99 );
+		add_filter( 'body_class',       array( $this, 'filter_body_class' ), 999 );
+		add_filter( 'template_include', array( $this, 'filter_template_include' ), 999 );
 	}
 
 	public function filter_body_class( $class ) {
@@ -32,11 +33,14 @@ class QM_Collector_Theme extends QM_Collector {
 		return $class;
 	}
 
+	public function filter_template_include( $template_path ) {
+		$this->data['template_path'] = $template_path;
+		return $template_path;
+	}
+
 	public function process() {
 
-		global $template;
-
-		$template_path        = QM_Util::standard_dir( $template );
+		$template_path        = QM_Util::standard_dir( $this->data['template_path'] );
 		$stylesheet_directory = QM_Util::standard_dir( get_stylesheet_directory() );
 		$template_directory   = QM_Util::standard_dir( get_template_directory() );
 		$theme_directory      = QM_Util::standard_dir( get_theme_root() );
