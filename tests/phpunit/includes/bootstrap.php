d23145b9036234ef914822ceaac12df5563c21dd
John Blackbourn <johnbillion@gmail.com>
2015-10-17 00:48:21 +0100

    Updated test infrastructure.

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -15,5 +15,4 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
 
-require dirname( __FILE__ ) . '/compat.php';
 require dirname( __FILE__ ) . '/qm-test.php';
