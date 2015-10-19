126514e80534c318aa129cbb0670e4834c0ad9c8
John Blackbourn <johnbillion@gmail.com>
2015-10-19 02:31:43 +0100

    Overview information in header output.

---
+++ output/headers/overview.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Copyright 2009-2015 John Blackbourn
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+class QM_Output_Headers_Overview extends QM_Output_Headers {
+
+	public function get_output() {
+
+		$data = $this->collector->get_data();
+		$headers = array();
+
+		$headers['time'] = number_format_i18n( $data['time'], 4 );
+		$headers['time_usage'] = sprintf(
+			__( '%1$s%% of %2$ss limit', 'query-monitor' ),
+			number_format_i18n( $data['time_usage'], 1 ),
+			number_format_i18n( $data['time_limit'] )
+		);
+
+		if ( ! empty( $data['memory'] ) ) {
+			$headers['memory'] = sprintf(
+				__( '%s kB', 'query-monitor' ),
+				number_format_i18n( $data['memory'] / 1024 )
+			);
+			$headers['memory_usage'] = sprintf(
+				__( '%1$s%% of %2$s kB limit', 'query-monitor' ),
+				number_format_i18n( $data['memory_usage'], 1 ),
+				number_format_i18n( $data['memory_limit'] / 1024 )
+			);
+		}
+
+		return $headers;
+
+	}
+
+}
+
+function register_qm_output_headers_overview( array $output, QM_Collectors $collectors ) {
+	if ( $collector = $collectors::get( 'overview' ) ) {
+		$output['overview'] = new QM_Output_Headers_Overview( $collector );
+	}
+	return $output;
+}
+
+add_filter( 'qm/outputter/headers', 'register_qm_output_headers_overview', 10, 2 );
