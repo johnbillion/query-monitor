80900db83bb5b70afb96ed1d9b7108b80ae1322b
John Blackbourn <john@johnblackbourn.com>
2016-03-27 02:05:11 +0100

    Add a separator between admin toolbar title items when multiple WPDBs are in use.

--- output/html/db_queries.php
+++ output/html/db_queries.php
@@ -403,9 +403,10 @@ class QM_Output_Html_DB_Queries extends QM_Output_Html {
 		$data = $this->collector->get_data();
 
 		if ( isset( $data['dbs'] ) ) {
-			foreach ( $data['dbs'] as $db ) {
+			foreach ( $data['dbs'] as $key => $db ) {
 				$title[] = sprintf(
-					_x( '%s<small>S</small>', 'database query time', 'query-monitor' ),
+					_x( '%s%s<small>S</small>', 'database query time', 'query-monitor' ),
+					( count( $data['dbs'] ) > 1 ? '&bull;&nbsp;&nbsp;&nbsp;' : '' ),
 					number_format_i18n( $db->total_time, 4 )
 				);
 				$title[] = sprintf(
