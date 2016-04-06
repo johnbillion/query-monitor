541499d0d837b56759a117b95d6d84073e22026c
John Blackbourn <john@johnblackbourn.com>
2016-04-06 23:11:31 +0100

    Remove unnecessary complexity for tables that use equal column widths.

--- output/html/conditionals.php
+++ output/html/conditionals.php
@@ -27,7 +27,6 @@ class QM_Output_Html_Conditionals extends QM_Output_Html {
 
 		$cols = 6;
 		$i = 0;
-		$w = floor( 100 / $cols );
 
 		echo '<div class="qm" id="' . esc_attr( $this->collector->id() ) . '">';
 		echo '<table cellspacing="0">';
@@ -43,7 +42,7 @@ class QM_Output_Html_Conditionals extends QM_Output_Html {
 			if ( 1 === $i % $cols ) {
 				echo '<tr>';
 			}
-			echo '<td class="qm-ltr qm-true" width="' . absint( $w ) . '%">' . esc_html( $cond ) . '()&nbsp;&#x2713;</td>';
+			echo '<td class="qm-ltr qm-true">' . esc_html( $cond ) . '()&nbsp;&#x2713;</td>';
 			if ( 0 === $i % $cols ) {
 				echo '</tr>';
 			}
@@ -54,7 +53,7 @@ class QM_Output_Html_Conditionals extends QM_Output_Html {
 			if ( 1 === $i % $cols ) {
 				echo '<tr>';
 			}
-			echo '<td class="qm-ltr qm-false" width="' . absint( $w ) . '%">' . esc_html( $cond ) . '()</td>';
+			echo '<td class="qm-ltr qm-false">' . esc_html( $cond ) . '()</td>';
 			if ( 0 === $i % $cols ) {
 				echo '</tr>';
 			}
