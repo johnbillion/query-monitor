42e0be2d93afc9a30a3bdc1ca2ba940284740f0a
John Blackbourn <johnbillion@gmail.com>
2017-11-08 16:47:45 +0000

    Rename the error handling constant.

--- collectors/php_errors.php
+++ collectors/php_errors.php
@@ -40,10 +40,10 @@ class QM_Collector_PHP_Errors extends QM_Collector {
 	}
 
 	public function __construct() {
-		if ( defined( 'QM_PHP_ERRORS_DISABLED' ) and QM_PHP_ERRORS_DISABLED ) {
+		if ( defined( 'QM_DISABLE_ERROR_HANDLER' ) and QM_DISABLE_ERROR_HANDLER ) {
 			return;
 		}
-		
+
 		parent::__construct();
 		set_error_handler( array( $this, 'error_handler' ) );
 		register_shutdown_function( array( $this, 'shutdown_handler' ) );
