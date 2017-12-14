27b094c213e66cd4f00607648ed660a149602545
John Blackbourn <johnbillion@gmail.com>
2017-12-14 00:37:13 +0000

    Convert the Admin Screen output to use an Underscore template.

--- output/html/admin.php
+++ output/html/admin.php
@@ -21,71 +21,51 @@ class QM_Output_Html_Admin extends QM_Output_Html {
 		add_filter( 'qm/output/menus', array( $this, 'admin_menu' ), 60 );
 	}
 
-	public function output() {
-
-		$data = $this->collector->get_data();
-
-		if ( empty( $data['current_screen'] ) ) {
-			return;
-		}
-
-		echo '<div class="qm qm-half" id="' . esc_attr( $this->collector->id() ) . '">';
-		echo '<table cellspacing="0">';
-		echo '<caption>' . esc_html( $this->collector->name() ) . '</caption>';
-		echo '<thead class="screen-reader-text">';
-		echo '<tr>';
-		echo '<th>' . esc_html__( 'Data', 'query-monitor' ) . '</th>';
-		echo '<th>' . esc_html__( 'Property', 'query-monitor' ) . '</th>';
-		echo '<th>' . esc_html__( 'Value', 'query-monitor' ) . '</th>';
-		echo '</tr>';
-		echo '</thead>';
-		echo '<tbody class="qm-group">';
-
-		$first = true;
-
-		foreach ( $data['current_screen'] as $key => $value ) {
-			echo '<tr>';
-
-			if ( $first ) {
-				echo '<th class="qm-ltr" rowspan="' . count( $data['current_screen'] ) . '">get_current_screen()</th>';
-			}
-
-			echo '<td>' . esc_html( $key ) . '</td>';
-			echo '<td>' . esc_html( $value ) . '</td>';
-			echo '</tr>';
-
-			$first = false;
-		}
-
-		echo '</tbody>';
-
-		echo '<tbody>';
-		echo '<tr>';
-		echo '<th class="qm-ltr">$pagenow</th>';
-		echo '<td colspan="2">' . esc_html( $data['pagenow'] ) . '</td>';
-		echo '</tr>';
-
-		if ( ! empty( $data['list_table'] ) ) {
-
-			echo '<tr>';
-			echo '<th rowspan="2">' . esc_html__( 'Column Filters', 'query-monitor' ) . '</th>';
-			echo '<td colspan="2">' . $data['list_table_markup']['columns_filter'] . '</td>'; // WPCS: XSS ok;
-			echo '</tr>';
-			echo '<tr>';
-			echo '<td colspan="2">' . $data['list_table_markup']['sortables_filter'] . '</td>'; // WPCS: XSS ok;
-			echo '</tr>';
-
-			echo '<tr>';
-			echo '<th>' . esc_html__( 'Column Action', 'query-monitor' ) . '</th>';
-			echo '<td colspan="2">' . $data['list_table_markup']['column_action'] . '</td>'; // WPCS: XSS ok;
-			echo '</tr>';
-
-		}
-
-		echo '</tbody>';
-		echo '</table>';
-		echo '</div>';
-
+	public function template() {
+		?>
+		<div class="qm qm-half" id="{{ data._collector.id }}">
+			<table cellspacing="0">
+				<caption>{{ data._collector.name }}</caption>
+				<thead class="screen-reader-text">
+					<tr>
+						<th><?php esc_html_e( 'Data', 'query-monitor' ); ?></th>
+						<th><?php esc_html_e( 'Property', 'query-monitor' ); ?></th>
+						<th><?php esc_html_e( 'Value', 'query-monitor' ); ?></th>
+					</tr>
+				</thead>
+				<tbody class="qm-group">
+					<# var first = true; for ( var key in data.current_screen ) { #>
+						<tr>
+							<# if ( first ) { #>
+								<th class="qm-ltr" rowspan="{{ Object.keys( data.current_screen ).length }}">get_current_screen()</th>
+							<# } #>
+							<td>{{ key }}</td>
+							<td>{{ data.current_screen[ key ] }}</td>
+						</tr>
+					<# first = false; } #>
+				</tbody>
+				<tbody>
+					<tr>
+						<th class="qm-ltr">$pagenow</th>
+						<td colspan="2">{{ data.pagenow }}</td>
+					</tr>
+					<# if ( data.list_table ) { #>
+						<tr>
+							<th rowspan="2"><?php esc_html_e( 'Column Filters', 'query-monitor' ); ?></th>
+							<td colspan="2">{{{ data.list_table_markup.columns_filter }}}</td>
+						</tr>
+						<tr>
+							<td colspan="2">{{{ data.list_table_markup.sortables_filter }}}</td>
+						</tr>
+						<tr>
+							<th><?php esc_html_e( 'Column Action', 'query-monitor' ); ?></th>
+							<td colspan="2">{{{ data.list_table_markup.column_action }}}</td>
+						</tr>
+					<# } #>
+				</tbody>
+			</table>
+		</div>
+		<?php
 	}
 
 }
