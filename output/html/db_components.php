b627499364f147b475a3e582ff03aa222f15b5b5
John Blackbourn <johnbillion@gmail.com>
2018-03-23 21:29:32 +0000

    Don't show the Queries by Component panel if the data isn't available.

--- output/html/db_components.php
+++ output/html/db_components.php
@@ -25,7 +25,7 @@ class QM_Output_Html_DB_Components extends QM_Output_Html {
 
 		$data = $this->collector->get_data();
 
-		if ( empty( $data['types'] ) ) {
+		if ( empty( $data['types'] ) || empty( $data['times'] ) ) {
 			return;
 		}
 
@@ -37,79 +37,58 @@ class QM_Output_Html_DB_Components extends QM_Output_Html {
 		echo '<caption>' . esc_html( $this->collector->name() ) . '</caption>';
 		echo '<thead>';
 
-		if ( ! empty( $data['times'] ) ) {
-			echo '<tr>';
-			echo '<th scope="col">' . esc_html__( 'Component', 'query-monitor' ) . '</th>';
+		echo '<tr>';
+		echo '<th scope="col">' . esc_html__( 'Component', 'query-monitor' ) . '</th>';
 
-			foreach ( $data['types'] as $type_name => $type_count ) {
-				echo '<th scope="col" class="qm-num qm-sortable-column">';
-				echo $this->build_sorter( $type_name ); // WPCS: XSS ok;
-				echo '</th>';
-			}
-
-			echo '<th scope="col" class="qm-num qm-sorted-desc qm-sortable-column">';
-			echo $this->build_sorter( __( 'Time', 'query-monitor' ) ); // WPCS: XSS ok;
+		foreach ( $data['types'] as $type_name => $type_count ) {
+			echo '<th scope="col" class="qm-num qm-sortable-column">';
+			echo $this->build_sorter( $type_name ); // WPCS: XSS ok;
 			echo '</th>';
-			echo '</tr>';
 		}
+
+		echo '<th scope="col" class="qm-num qm-sorted-desc qm-sortable-column">';
+		echo $this->build_sorter( __( 'Time', 'query-monitor' ) ); // WPCS: XSS ok;
+		echo '</th>';
+		echo '</tr>';
 
 		echo '</thead>';
 
-		if ( ! empty( $data['times'] ) ) {
+		echo '<tbody>';
 
-			echo '<tbody>';
-
-			foreach ( $data['times'] as $row ) {
-				$total_time  += $row['ltime'];
-
-				echo '<tr>';
-				echo '<th scope="row"><a href="#" class="qm-filter-trigger" data-qm-target="db_queries-wpdb" data-qm-filter="component" data-qm-value="' . esc_attr( $row['component'] ) . '">' . esc_html( $row['component'] ) . '</a></th>';
-
-				foreach ( $data['types'] as $type_name => $type_count ) {
-					if ( isset( $row['types'][ $type_name ] ) ) {
-						echo '<td class="qm-num">' . esc_html( number_format_i18n( $row['types'][ $type_name ] ) ) . '</td>';
-					} else {
-						echo '<td class="qm-num">&nbsp;</td>';
-					}
-				}
-
-				echo '<td class="qm-num" data-qm-sort-weight="' . esc_attr( $row['ltime'] ) . '">' . esc_html( number_format_i18n( $row['ltime'], 4 ) ) . '</td>';
-				echo '</tr>';
-
-			}
-
-			echo '</tbody>';
-			echo '<tfoot>';
-
-			$total_stime = number_format_i18n( $total_time, 4 );
+		foreach ( $data['times'] as $row ) {
+			$total_time  += $row['ltime'];
 
 			echo '<tr>';
-			echo '<td>' . esc_html__( 'Total', 'query-monitor' ) . '</td>';
+			echo '<th scope="row"><a href="#" class="qm-filter-trigger" data-qm-target="db_queries-wpdb" data-qm-filter="component" data-qm-value="' . esc_attr( $row['component'] ) . '">' . esc_html( $row['component'] ) . '</a></th>';
 
 			foreach ( $data['types'] as $type_name => $type_count ) {
-				echo '<td class="qm-num">' . esc_html( number_format_i18n( $type_count ) ) . '</td>';
+				if ( isset( $row['types'][ $type_name ] ) ) {
+					echo '<td class="qm-num">' . esc_html( number_format_i18n( $row['types'][ $type_name ] ) ) . '</td>';
+				} else {
+					echo '<td class="qm-num">&nbsp;</td>';
+				}
 			}
 
-			echo '<td class="qm-num">' . esc_html( $total_stime ) . '</td>';
+			echo '<td class="qm-num" data-qm-sort-weight="' . esc_attr( $row['ltime'] ) . '">' . esc_html( number_format_i18n( $row['ltime'], 4 ) ) . '</td>';
 			echo '</tr>';
-			echo '</tfoot>';
-
-		} else {
-
-			echo '<tbody>';
-			echo '<tr>';
-			echo '<td colspan="' . esc_attr( $span ) . '" style="text-align:center !important">';
-			echo '<em>' . esc_html__( 'Unknown', 'query-monitor' ) . '</em>';
-			printf(
-				'&nbsp;<span class="qm-info">(<a href="%s" target="_blank">%s</a>)</span>',
-				'https://github.com/johnbillion/query-monitor/wiki/db.php-Symlink',
-				esc_html__( 'Help', 'query-monitor' )
-			);
-			echo '</td>';
-			echo '</tr>';
-			echo '</tbody>';
 
 		}
+
+		echo '</tbody>';
+		echo '<tfoot>';
+
+		$total_stime = number_format_i18n( $total_time, 4 );
+
+		echo '<tr>';
+		echo '<td>' . esc_html__( 'Total', 'query-monitor' ) . '</td>';
+
+		foreach ( $data['types'] as $type_name => $type_count ) {
+			echo '<td class="qm-num">' . esc_html( number_format_i18n( $type_count ) ) . '</td>';
+		}
+
+		echo '<td class="qm-num">' . esc_html( $total_stime ) . '</td>';
+		echo '</tr>';
+		echo '</tfoot>';
 
 		echo '</table>';
 		echo '</div>';
@@ -117,6 +96,11 @@ class QM_Output_Html_DB_Components extends QM_Output_Html {
 	}
 
 	public function admin_menu( array $menu ) {
+		$data = $this->collector->get_data();
+
+		if ( empty( $data['types'] ) || empty( $data['times'] ) ) {
+			return $menu;
+		}
 
 		if ( $dbq = QM_Collectors::get( 'db_queries' ) ) {
 			$dbq_data = $dbq->get_data();
