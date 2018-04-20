d1973cd9a36daaf2e2aed9e29b3cb0d694573527
John Blackbourn <johnbillion@gmail.com>
2018-04-20 17:10:23 +0200

    Add infrastructure for Sass.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: {
+					'assets/query-monitor.css': 'assets/query-monitor.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', [
+		'watch'
+	]);
+};
