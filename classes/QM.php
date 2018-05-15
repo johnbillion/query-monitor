0189021832389af1d6b1971aa8eb579ce42e9a3a
John Blackbourn <johnbillion@gmail.com>
2018-05-15 15:05:04 +0200

    Switch to using actions for the `QM` interface for logging.

--- classes/QM.php
+++ classes/QM.php
@@ -8,43 +8,35 @@
 class QM {
 
 	public static function emergency( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->emergency( $message, $context );
+		do_action( 'qm/emergency', $message, $context );
 	}
 
 	public static function alert( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->alert( $message, $context );
+		do_action( 'qm/alert', $message, $context );
 	}
 
 	public static function critical( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->critical( $message, $context );
+		do_action( 'qm/critical', $message, $context );
 	}
 
 	public static function error( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->error( $message, $context );
+		do_action( 'qm/error', $message, $context );
 	}
 
 	public static function warning( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->warning( $message, $context );
+		do_action( 'qm/warning', $message, $context );
 	}
 
 	public static function notice( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->notice( $message, $context );
+		do_action( 'qm/notice', $message, $context );
 	}
 
 	public static function info( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->info( $message, $context );
+		do_action( 'qm/info', $message, $context );
 	}
 
 	public static function debug( $message, array $context = array() ) {
-		$logger = QM_Collectors::get( 'logger' );
-		$logger->debug( $message, $context );
+		do_action( 'qm/debug', $message, $context );
 	}
 
 	public static function log( $level, $message, array $context = array() ) {
