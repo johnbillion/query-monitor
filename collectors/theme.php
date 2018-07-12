bee172bb4de079d8442680a5319ecc80ab680ef4
Jason Busse <jasonb@stellarbluetechnologies.com>
2018-07-12 01:56:34 -0500

    Template Part file string normalization

--- collectors/theme.php
+++ collectors/theme.php
@@ -121,6 +121,7 @@ class QM_Collector_Theme extends QM_Collector {
 			$this->data['template_hierarchy']   = array_unique( $this->data['template_hierarchy'] );
 
 			foreach ( get_included_files() as $file ) {
+				$file = wp_normalize_path( $file );
 				$filename = str_replace( array(
 					$stylesheet_directory,
 					$template_directory,
