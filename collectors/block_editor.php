e9564c8dbbc8d661be58d3bb13f3c5ee2798d441
John Blackbourn <johnbillion@gmail.com>
2018-11-22 22:27:34 +0100

    Import the new Block Editor panel.

---
+++ collectors/block_editor.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Block editor (née Gutenberg) collector.
+ *
+ * @package query-monitor
+ */
+
+class QM_Collector_Block_Editor extends QM_Collector {
+
+	public $id = 'block_editor';
+
+	public function name() {
+		return __( 'Blocks', 'query-monitor' );
+	}
+
+	public function process() {
+		$this->data['block_editor_enabled'] = self::wp_block_editor_enabled();
+
+		if ( ! is_singular() ) {
+			return;
+		}
+
+		$block_type_registry = WP_Block_Type_Registry::get_instance();
+
+		$this->data['post_has_blocks']    = self::wp_has_blocks( get_post()->post_content );
+		$this->data['post_blocks']        = self::wp_parse_blocks( get_post()->post_content );
+		$this->data['all_dynamic_blocks'] = self::wp_get_dynamic_block_names();
+
+		if ( $this->data['post_has_blocks'] ) {
+			foreach ( $this->data['post_blocks'] as $i => $block ) {
+				$block_type = $block_type_registry->get_registered( $block['blockName'] );
+				$dynamic    = false;
+				$callback   = null;
+
+				if ( $block_type && $block_type->is_dynamic() ) {
+					$dynamic  = true;
+					$callback = QM_Util::populate_callback( array(
+						'function' => $block_type->render_callback,
+					) );
+				}
+
+				$this->data['post_blocks'][ $i ]['dynamic']  = $dynamic;
+				$this->data['post_blocks'][ $i ]['callback'] = $callback;
+			}
+		}
+	}
+
+	protected static function wp_block_editor_enabled() {
+		return ( function_exists( 'parse_blocks' ) || function_exists( 'gutenberg_parse_blocks' ) );
+	}
+
+	protected static function wp_has_blocks( $content ) {
+		if ( function_exists( 'has_blocks' ) ) {
+			return has_blocks( $content );
+		} elseif ( function_exists( 'gutenberg_has_blocks' ) ) {
+			return gutenberg_has_blocks( $content );
+		}
+
+		return false;
+	}
+
+	protected static function wp_parse_blocks( $content ) {
+		if ( function_exists( 'parse_blocks' ) ) {
+			return parse_blocks( $content );
+		} elseif ( function_exists( 'gutenberg_parse_blocks' ) ) {
+			return gutenberg_parse_blocks( $content );
+		}
+
+		return null;
+	}
+
+	protected static function wp_get_dynamic_block_names() {
+		return get_dynamic_block_names();
+	}
+
+}
+
+function register_qm_collector_block_editor( array $collectors, QueryMonitor $qm ) {
+	$collectors['block_editor'] = new QM_Collector_Block_Editor;
+	return $collectors;
+}
+
+add_filter( 'qm/collectors', 'register_qm_collector_block_editor', 10, 2 );
---
+++ output/html/block_editor.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Block editor data output for HTML pages.
+ *
+ * @package query-monitor
+ */
+
+class QM_Output_Html_Block_Editor extends QM_Output_Html {
+
+	public function __construct( QM_Collector $collector ) {
+		parent::__construct( $collector );
+		add_filter( 'qm/output/menus', array( $this, 'admin_menu' ), 55 );
+	}
+
+	public function output() {
+		$data = $this->collector->get_data();
+		$i    = 0;
+
+		if ( empty( $data['block_editor_enabled'] ) || empty( $data['post_blocks'] ) ) {
+			return;
+		}
+
+		if ( ! $data['post_has_blocks'] ) {
+			$this->before_non_tabular_output();
+
+			echo '<div class="qm-section">';
+			echo '<div class="qm-notice">';
+			echo '<p>';
+			esc_html_e( 'This post contains no blocks.', 'query-monitor' );
+			echo '</p>';
+			echo '</div>';
+			echo '</div>';
+
+			$this->after_non_tabular_output();
+
+			return;
+		}
+
+		$this->before_tabular_output();
+
+		echo '<thead>';
+		echo '<tr>';
+		echo '<th scope="col">#</th>';
+		echo '<th scope="col">' . esc_html__( 'Block Name', 'query-monitor' ) . '</th>';
+		echo '<th scope="col">' . esc_html__( 'Attributes', 'query-monitor' ) . '</th>';
+		echo '<th scope="col">' . esc_html__( 'Render Callback', 'query-monitor' ) . '</th>';
+		echo '<th scope="col">' . esc_html__( 'Inner HTML', 'query-monitor' ) . '</th>';
+		echo '</tr>';
+		echo '</thead>';
+
+		echo '<tbody>';
+
+		foreach ( $data['post_blocks'] as $block ) {
+			$inner_html = trim( $block['innerHTML'] );
+
+			// Don't display empty blocks caused by two consecutive line breaks in content
+			if ( ! $block['blockName'] && ! $inner_html ) {
+				continue;
+			}
+
+			$block_error = ( empty( $block['blockName'] ) && ! empty( $inner_html ) );
+			$row_class = '';
+
+			if ( $block_error ) {
+				$row_class = 'qm-warn';
+			}
+
+			echo '<tr class="' . esc_attr( $row_class ) . '">';
+
+			echo '<th scope="row" class="qm-row-num qm-num">' . absint( ++$i ) . '</th>';
+
+			echo '<td class="qm-row-block-name">';
+
+			if ( $block['blockName'] ) {
+				echo esc_html( $block['blockName'] );
+			} elseif ( $block_error ) {
+				echo '<em><span class="dashicons dashicons-warning" aria-hidden="true"></span>' . esc_html__( 'None', 'query-monitor' ) . '</em>';
+			} else {
+				echo '<em>' . esc_html__( 'None', 'query-monitor' ) . '</em>';
+			}
+
+			if ( 'core/block' === $block['blockName'] && ! empty( $block['attrs']['ref'] ) ) {
+				echo '<br>';
+				echo '<a href="' . esc_url( get_edit_post_link( $block['attrs']['ref'] ) ) . '" class="qm-link">' . esc_html__( 'Edit Block', 'query-monitor' ) . '</a>';
+			}
+
+			echo '</td>';
+
+			echo '<td class="qm-row-block-attrs">';
+			if ( $block['attrs'] ) {
+				$json = json_encode( $block['attrs'], JSON_PRETTY_PRINT );
+				echo '<pre>' . esc_html( $json ) . '</pre>';
+			}
+			echo '</td>';
+
+			if ( isset( $block['callback']['error'] ) ) {
+				$class = ' qm-warn';
+			} else {
+				$class = '';
+			}
+
+			if ( $block['dynamic'] ) {
+				if ( isset( $block['callback']['file'] ) ) {
+					if ( self::has_clickable_links() ) {
+						echo '<td class="qm-nowrap qm-ltr' . esc_attr( $class ) . '">';
+						echo self::output_filename( $block['callback']['name'], $block['callback']['file'], $block['callback']['line'] ); // WPCS: XSS ok.
+						echo '</td>';
+					} else {
+						echo '<td class="qm-nowrap qm-ltr qm-has-toggle' . esc_attr( $class ) . '"><ol class="qm-toggler">';
+						echo self::build_toggler(); // WPCS: XSS ok;
+						echo '<li>';
+						echo self::output_filename( $block['callback']['name'], $block['callback']['file'], $block['callback']['line'] ); // WPCS: XSS ok.
+						echo '</li>';
+						echo '</ol></td>';
+					}
+				} else {
+					echo '<td class="qm-ltr qm-nowrap' . esc_attr( $class ) . '">';
+					echo '<code>' . esc_html( $block['callback']['name'] ) . '</code>';
+
+					if ( isset( $block['callback']['error'] ) ) {
+						echo '<br>';
+						echo esc_html( sprintf(
+							/* translators: %s: Error message text */
+							__( 'Error: %s', 'query-monitor' ),
+							$block['callback']['error']->get_error_message()
+						) );
+					}
+
+					echo '</td>';
+				}
+			} else {
+				echo '<td></td>';
+			}
+
+			echo '<td class="qm-row-block-html">';
+			if ( $block['innerHTML'] ) {
+				echo esc_html( $block['innerHTML'] );
+			}
+			echo '</td>';
+
+			echo '</tr>';
+		}
+
+		echo '</tbody>';
+
+		$this->after_tabular_output();
+	}
+
+	public function admin_menu( array $menu ) {
+		$data = $this->collector->get_data();
+
+		if ( empty( $data['block_editor_enabled'] ) || empty( $data['post_blocks'] ) ) {
+			return $menu;
+		}
+
+		$menu[] = $this->menu( array(
+			'title' => esc_html__( 'Blocks', 'query-monitor' ),
+		) );
+
+		return $menu;
+	}
+
+}
+
+function register_qm_output_html_block_editor( array $output, QM_Collectors $collectors ) {
+	if ( $collector = QM_Collectors::get( 'block_editor' ) ) {
+		$output['block_editor'] = new QM_Output_Html_Block_Editor( $collector );
+	}
+	return $output;
+}
+
+add_filter( 'qm/outputter/html', 'register_qm_output_html_block_editor', 60, 2 );
