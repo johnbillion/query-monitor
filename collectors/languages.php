fde277c85e3eb7a723195e5046e568531e99df25
John Blackbourn <johnbillion@gmail.com>
2018-11-27 11:47:56 +0000

    First pass at support for Jed translations in WordPress 5.0. See https://core.trac.wordpress.org/ticket/45425.

--- collectors/languages.php
+++ collectors/languages.php
@@ -18,6 +18,7 @@ class QM_Collector_Languages extends QM_Collector {
 		parent::__construct();
 
 		add_filter( 'override_load_textdomain', array( $this, 'log_file_load' ), 99, 3 );
+		add_filter( 'load_script_textdomain_file', array( $this, 'log_script_file_load' ), 99, 3 );
 
 	}
 
@@ -78,12 +79,71 @@ class QM_Collector_Languages extends QM_Collector {
 		$this->data['languages'][ $domain ][] = array(
 			'caller' => $caller,
 			'domain' => $domain,
-			'mofile' => $mofile,
+			'file'   => $mofile,
 			'found'  => file_exists( $mofile ) ? filesize( $mofile ) : false,
+			'handle' => null,
+			'type'   => 'gettext',
 		);
 
 		return $override;
 
+	}
+
+	/**
+	 * Filters the file path for loading script translations for the given script handle and textdomain.
+	 *
+	 * @param string|false $file   Path to the translation file to load. False if there isn't one.
+	 * @param string       $handle Name of the script to register a translation domain to.
+	 * @param string       $domain The textdomain.
+	 *
+	 * @return string|false Path to the translation file to load. False if there isn't one.
+	 */
+	public function log_script_file_load( $file, $handle, $domain ) {
+		if ( 'query-monitor' === $domain && self::hide_qm() ) {
+			return $file;
+		}
+
+		$trace    = new QM_Backtrace;
+		$filtered = $trace->get_filtered_trace();
+		$caller   = array();
+
+		foreach ( $filtered as $i => $item ) {
+
+			if ( in_array( $item['function'], array(
+				'load_script_translation',
+			), true ) ) {
+				$caller = $item;
+				$display = $i + 1;
+				if ( isset( $filtered[ $display ] ) ) {
+					$caller['display'] = $filtered[ $display ]['display'];
+				}
+				break;
+			}
+		}
+
+		if ( empty( $caller ) ) {
+			if ( isset( $filtered[1] ) ) {
+				$caller = $filtered[1];
+			} else {
+				$caller = $filtered[0];
+			}
+		}
+
+		if ( ! isset( $caller['file'] ) && isset( $filtered[0]['file'] ) && isset( $filtered[0]['line'] ) ) {
+			$caller['file'] = $filtered[0]['file'];
+			$caller['line'] = $filtered[0]['line'];
+		}
+
+		$this->data['languages'][ $domain ][] = array(
+			'caller' => $caller,
+			'domain' => $domain . ' / ' . $handle,
+			'file'   => $file,
+			'found'  => ( $file && file_exists( $file ) ) ? filesize( $file ) : false,
+			'handle' => $handle,
+			'type'   => 'jed',
+		);
+
+		return $file;
 	}
 
 }
