f5c2ab0b5b2dbf5e4b872d19d1f4cb0ea6512c44
John Blackbourn <johnbillion@gmail.com>
2019-01-02 16:03:12 +0100

    Remove some indentation.

--- collectors/assets.php
+++ collectors/assets.php
@@ -97,58 +97,58 @@ class QM_Collector_Assets extends QM_Collector {
 			$all_dependents   = array();
 
 			foreach ( $positions as $position ) {
-				if ( ! empty( $this->data[ $position ][ $type ] ) ) {
-					$handles = $this->data[ $position ][ $type ];
+				if ( empty( $this->data[ $position ][ $type ] ) ) {
+					continue;
+				}
 
-					foreach ( $handles as $handle ) {
-						$dependency = $raw->query( $handle );
+				foreach ( $this->data[ $position ][ $type ] as $handle ) {
+					$dependency = $raw->query( $handle );
 
-						if ( ! $dependency ) {
-							continue;
-						}
-
-						$all_dependencies = array_merge( $all_dependencies, $dependency->deps );
-						$dependents       = $this->get_dependents( $dependency, $raw );
-						$all_dependents   = array_merge( $all_dependents, $dependents );
-
-						list( $host, $source, $local ) = $this->get_dependency_data( $dependency, $raw, $type );
-
-						if ( empty( $dependency->ver ) ) {
-							$ver = '';
-						} else {
-							$ver = $dependency->ver;
-						}
-
-						$warning = ! in_array( $handle, $raw->done, true );
-
-						if ( is_wp_error( $source ) ) {
-							$display = $source->get_error_message();
-						} else {
-							$display = ltrim( str_replace( home_url(), '', remove_query_arg( 'ver', $source ) ), '/' );
-						}
-
-						$dependencies = $dependency->deps;
-
-						foreach ( $dependencies as & $dep ) {
-							if ( ! $raw->query( $dep ) ) {
-								/* translators: %s: Script or style dependency name */
-								$dep = sprintf( __( '%s (missing)', 'query-monitor' ), $dep );
-							}
-						}
-
-						$this->data['assets'][ $position ][ $type ][] = array(
-							'position'     => $position,
-							'handle'       => $handle,
-							'host'         => $host,
-							'source'       => $source,
-							'local'        => $local,
-							'ver'          => $ver,
-							'warning'      => $warning,
-							'display'      => $display,
-							'dependents'   => $dependents,
-							'dependencies' => $dependencies,
-						);
+					if ( ! $dependency ) {
+						continue;
 					}
+
+					$all_dependencies = array_merge( $all_dependencies, $dependency->deps );
+					$dependents       = $this->get_dependents( $dependency, $raw );
+					$all_dependents   = array_merge( $all_dependents, $dependents );
+
+					list( $host, $source, $local ) = $this->get_dependency_data( $dependency, $raw, $type );
+
+					if ( empty( $dependency->ver ) ) {
+						$ver = '';
+					} else {
+						$ver = $dependency->ver;
+					}
+
+					$warning = ! in_array( $handle, $raw->done, true );
+
+					if ( is_wp_error( $source ) ) {
+						$display = $source->get_error_message();
+					} else {
+						$display = ltrim( str_replace( home_url(), '', remove_query_arg( 'ver', $source ) ), '/' );
+					}
+
+					$dependencies = $dependency->deps;
+
+					foreach ( $dependencies as & $dep ) {
+						if ( ! $raw->query( $dep ) ) {
+							/* translators: %s: Script or style dependency name */
+							$dep = sprintf( __( '%s (missing)', 'query-monitor' ), $dep );
+						}
+					}
+
+					$this->data['assets'][ $position ][ $type ][] = array(
+						'position'     => $position,
+						'handle'       => $handle,
+						'host'         => $host,
+						'source'       => $source,
+						'local'        => $local,
+						'ver'          => $ver,
+						'warning'      => $warning,
+						'display'      => $display,
+						'dependents'   => $dependents,
+						'dependencies' => $dependencies,
+					);
 				}
 			}
 
