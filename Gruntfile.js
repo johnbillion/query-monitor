ff71a05dd931ecef16bdb07fe4e999b4add65b6c
John Blackbourn <johnbillion@gmail.com>
2019-02-13 22:27:52 +0000

    Alignment.

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,63 +17,63 @@ module.exports = function (grunt) {
 	config.pkg = pkg;
 
 	config.clean = {
-			main: [
-				'<%= wp_deploy.deploy.options.build_dir %>'
-			],
-			css: [
-				'assets/*.css'
-			]
+		main: [
+			'<%= wp_deploy.deploy.options.build_dir %>'
+		],
+		css: [
+			'assets/*.css'
+		]
 	};
 
 	config.copy = {
-			main: {
-				src: [
-					'**',
-					'!.*',
-					'!.git/**',
-					'!<%= wp_deploy.deploy.options.assets_dir %>/**',
-					'!<%= wp_deploy.deploy.options.build_dir %>/**',
-					'!README.md',
-					'!wiki/**',
-					ignored_gitignore,
-					ignored_gitattributes
-				],
-				dest: '<%= wp_deploy.deploy.options.build_dir %>/'
-			}
+		main: {
+			src: [
+				'**',
+				'!.*',
+				'!.git/**',
+				'!<%= wp_deploy.deploy.options.assets_dir %>/**',
+				'!<%= wp_deploy.deploy.options.build_dir %>/**',
+				'!README.md',
+				'!wiki/**',
+				ignored_gitignore,
+				ignored_gitattributes
+			],
+			dest: '<%= wp_deploy.deploy.options.build_dir %>/'
+		}
 	};
 
 	config.sass = {
-			dist: {
-				options: {
-					sourcemap: 'none',
-					style: 'expanded'
-				},
-				files: {
-					'assets/query-monitor-dark.css': 'assets/query-monitor-dark.scss',
-					'assets/query-monitor.css': 'assets/query-monitor.scss'
-				}
+		dist: {
+			options: {
+				sourcemap: 'none',
+				style: 'expanded'
+			},
+			files: {
+				'assets/query-monitor-dark.css': 'assets/query-monitor-dark.scss',
+				'assets/query-monitor.css': 'assets/query-monitor.scss'
 			}
+		}
 	};
 
 	config.watch = {
-			options: {
-				interval: 1000
-			},
-			css: {
-				files: '**/*.scss',
-				tasks: ['sass']
-			}
+		options: {
+			interval: 1000
+		},
+		css: {
+			files: '**/*.scss',
+			tasks: ['sass']
+		}
 	};
 
 	config.wp_deploy = {
-			deploy: {
-				options: {
-					svn_user: 'johnbillion',
-					plugin_slug: '<%= pkg.name %>',
-					build_dir: 'build',
-					assets_dir: 'assets-wp-repo'
-				}
+		deploy: {
+			options: {
+				svn_user: 'johnbillion',
+				plugin_slug: '<%= pkg.name %>',
+				build_dir: 'build',
+				assets_dir: 'assets-wp-repo'
 			}
+		}
 	};
 
 	grunt.initConfig(config);
