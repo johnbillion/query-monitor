6af28a8cacb89b3c6cf30d33deab11b8341c1ce2
John Blackbourn <johnbillion@gmail.com>
2019-03-15 23:29:49 +0000

    More docs and fewer docs.

--- tests/phpunit/wp-tests-config.php
+++ tests/phpunit/wp-tests-config.php
@@ -3,27 +3,18 @@
 $root     = dirname( dirname( __DIR__ ) );
 $composer = json_decode( file_get_contents( $root . '/composer.json' ), true );
 
-/* Path to the WordPress codebase you'd like to test. Add a forward slash in the end. */
+// Path to the WordPress codebase to test.
 define( 'ABSPATH', $root . '/' . $composer['extra']['wordpress-install-dir'] . '/' );
 
+// Dummy paths to get the theme tests working.
 define( 'WP_CONTENT_DIR', $root . '/content-dir' );
 define( 'WP_PLUGINS_DIR', $root . '/plugins-dir' );
 
-/*
- * Path to the theme to test with.
- *
- * The 'default' theme is symlinked from test/phpunit/data/themedir1/default into
- * the themes directory of the WordPress installation defined above.
- */
+// Path to the theme to test with.
 define( 'WP_DEFAULT_THEME', 'default' );
 
 // Test with WordPress debug mode (default).
 define( 'WP_DEBUG', true );
-
-// ** MySQL settings ** //
-
-// This configuration file will be used by the copy of WordPress being tested.
-// wordpress/wp-config.php will be ignored.
 
 // WARNING WARNING WARNING!
 // These tests will DROP ALL TABLES in the database with the prefix named below.
@@ -35,25 +26,8 @@ define( 'DB_HOST',     getenv( 'WP_TESTS_DB_HOST' ) ?: 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-
+// Test suite configuration.
 define( 'WP_TESTS_DOMAIN', 'example.org' );
 define( 'WP_TESTS_EMAIL', 'admin@example.org' );
 define( 'WP_TESTS_TITLE', 'Query Monitor Tests' );
-
 define( 'WP_PHP_BINARY', 'php' );
-
-define( 'WPLANG', '' );
