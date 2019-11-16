504721892bd576f526f295672bc7554c81c7261a
John Blackbourn <johnbillion@gmail.com>
2019-11-16 18:02:57 +0000

    Introduce a basic implementation of the block editor component.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import DBQueries from './output/html/db_queries.jsx';
 import DBDupes from './output/html/db_dupes.jsx';
 import DBCallers from './output/html/db_callers.jsx';
 import DBComponents from './output/html/db_components.jsx';
+import BlockEditor from './output/html/block_editor.jsx';
 
 jQuery(function($) {
 	ReactDOM.render(<Conditionals data={qm_data.conditionals.data} enabled={qm_data.conditionals.enabled} id="conditionals" /> , document.getElementById('qm-conditionals-container'));
@@ -26,4 +27,6 @@ jQuery(function($) {
 	ReactDOM.render(<DBDupes data={qm_data.db_dupes.data} enabled={qm_data.db_dupes.enabled} id="db_dupes" /> , document.getElementById('qm-db_dupes-container'));
 	ReactDOM.render(<DBCallers data={qm_data.db_callers.data} enabled={qm_data.db_callers.enabled} id="db_callers" /> , document.getElementById('qm-db_callers-container'));
 	ReactDOM.render(<DBComponents data={qm_data.db_components.data} enabled={qm_data.db_components.enabled} id="db_components" /> , document.getElementById('qm-db_components-container'));
+
+	ReactDOM.render(<BlockEditor data={qm_data.block_editor.data} enabled={qm_data.block_editor.enabled} id="block_editor" /> , document.getElementById('qm-block_editor-container'));
 } );
---
+++ output/html/block_editor.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+import Tabular from '../tabular.jsx';
+import PanelFooter from '../panel-footer.jsx';
+
+const { __, _x, _n, sprintf } = wp.i18n;
+
+class BlockEditor extends Component {
+
+	render() {
+		const data = this.props.data;
+
+		if ( ! data.post_blocks || ! data.post_blocks.length ) {
+			return null;
+		}
+
+		return (
+			<Tabular id={this.props.id}>
+				<thead>
+					<tr>
+						<th scope="col" role="columnheader">
+							#
+						</th>
+						<th scope="col">
+							{__( 'Block Name', 'query-monitor' )}
+						</th>
+						<th scope="col">
+							{__( 'Attributes', 'query-monitor' )}
+						</th>
+						<th scope="col">
+							{__( 'Render Callback', 'query-monitor' )}
+						</th>
+						<th scope="col">
+							{__( 'Render Time', 'query-monitor' )}
+						</th>
+						<th scope="col">
+							{__( 'Inner HTML', 'query-monitor' )}
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.post_blocks.map(function(block,i){
+						const show_attrs = ( ! Array.isArray( block.attrs ) || block.attrs.length > 0 );
+						return (
+							<tr>
+								<th scope="row" class="qm-row-num qm-num">{1+i}</th>
+								<td class="qm-ltr qm-wrap">{block.blockName}</td>
+								<td><pre class="qm-pre-wrap"><code>{show_attrs && JSON.stringify(block.attrs,null,2)}</code></pre></td>
+								<td>{block.dynamic && block.callback.name}</td>
+								<td>{block.dynamic && block.timing}</td>
+								<td><pre class="qm-pre-wrap"><code>{block.innerHTML}</code></pre></td>
+							</tr>
+						)
+					})}
+				</tbody>
+				<PanelFooter cols="6" label={__( 'Total:', 'Content blocks used', 'query-monitor' )} count={data.post_blocks.length}>
+				</PanelFooter>
+			</Tabular>
+		)
+	}
+
+}
+
+export default BlockEditor;
