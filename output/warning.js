984833f5b6e6b00c9bd5411c0cd2b5c3ece89f5f
John Blackbourn <johnbillion@gmail.com>
2020-03-14 10:33:51 +0000

    Introduce the warning component.

---
+++ output/warning.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Warning extends Component {
+
+	render() {
+		return (
+			<span class="dashicons dashicons-warning" aria-hidden="true"></span>
+		);
+	}
+
+}
+
+export default Warning;
