87e5954011867c119399bb4e20ff2429520445ae
John Blackbourn <johnbillion@gmail.com>
2020-03-15 17:31:01 +0000

    Extract the PHP output into its own component.

--- output/html/environment.js
+++ output/html/environment.js
@@ -1,123 +1,16 @@
 import React, { Component } from 'react';
 import NonTabular from '../non-tabular.js';
-import Warning from '../warning.js';
-import Toggler from '../toggler.js';
 import DB from '../db.js';
-import classnames from 'classnames';
 import { __, _x, _n, sprintf } from '@wordpress/i18n';
 
 class Environment extends Component {
 
 	render() {
 		const { data } = this.props;
-		const warning = data.php.old;
-		const classes = classnames( {
-			'qm-warn': warning,
-		} );
 
 		return (
 			<NonTabular id={this.props.id}>
-				<section>
-					<h3>
-						PHP
-					</h3>
-					<table>
-						<tbody>
-							<tr className={ classes }>
-								<th scope="row">
-									{ __( 'Version', 'query-monitor' ) }
-								</th>
-								<td>
-									{ warning ? (
-										<>
-											<Warning/>
-											{ data.php.version }
-											&nbsp;(<a href="https://wordpress.org/support/update-php/" class="qm-external-link">
-												{__( 'Help', 'query-monitor' )}
-											</a>)
-										</>
-									) : (
-										<>
-											{ data.php.version }
-										</>
-									)}
-								</td>
-							</tr>
-							<tr>
-								<th scope="row">
-									SAPI
-								</th>
-								<td>
-									{ data.php.sapi }
-								</td>
-							</tr>
-							<tr>
-								<th scope="row">
-									{ __( 'User', 'query-monitor' ) }
-								</th>
-								<td>
-									{ data.php.user || __( 'Unknown', 'query-monitor' ) }
-								</td>
-							</tr>
-							{Object.keys(data.php.variables).map(key =>
-								<tr key={key}>
-									<th scope="row">
-										{ key }
-									</th>
-									<td>
-										{ data.php.variables[ key ].after }
-									</td>
-								</tr>
-							)}
-							<tr>
-								<th scope="row">
-									{__( 'Error Reporting', 'query-monitor' )}
-								</th>
-								<td className="qm-has-toggle qm-ltr">
-									{ data.php.error_reporting }
-
-									<Toggler>
-										<ul className="qm-supplemental">
-											{Object.keys(data.php.error_levels).map(key =>
-												<li key={key}>
-													{ data.php.error_levels[ key ] ? (
-														<>
-															{key}&nbsp;&#x2713;
-														</>
-													):(
-														<span className="qm-false">
-															{key}
-														</span>
-													)}
-												</li>
-											)}
-										</ul>
-									</Toggler>
-								</td>
-							</tr>
-							{ data.php.extensions && (
-								<tr>
-									<th scope="row">
-										{__( 'Extensions', 'query-monitor' )}
-									</th>
-									<td className="qm-has-toggle qm-ltr">
-										{QM_i18n.number_format( Object.keys(data.php.extensions).length )}
-
-										<Toggler>
-											<ul className="qm-supplemental">
-												{Object.keys(data.php.extensions).map(key =>
-													<li key={key}>
-														{key} ({ data.php.extensions[ key ] || __( 'Unknown', 'query-monitor') })
-													</li>
-												)}
-											</ul>
-										</Toggler>
-									</td>
-								</tr>
-							)}
-						</tbody>
-					</table>
-				</section>
+				<PHP php={data.php}/>
 				{ data.db && (
 					<>
 						{Object.keys(data.db).map(key =>
---
+++ output/php.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import { __, _x, _n, sprintf } from '@wordpress/i18n';
+import Warning from '../warning.js';
+import Toggler from '../toggler.js';
+import classnames from 'classnames';
+
+class PHP extends Component {
+
+	render() {
+		const { php } = this.props;
+		const warning = php.old;
+		const classes = classnames( {
+			'qm-warn': warning,
+		} );
+
+		return (
+			<section>
+				<h3>
+					PHP
+				</h3>
+				<table>
+					<tbody>
+						<tr className={ classes }>
+							<th scope="row">
+								{ __( 'Version', 'query-monitor' ) }
+							</th>
+							<td>
+								{ warning ? (
+									<>
+										<Warning/>
+										{ php.version }
+										&nbsp;(<a href="https://wordpress.org/support/update-php/" class="qm-external-link">
+											{__( 'Help', 'query-monitor' )}
+										</a>)
+									</>
+								) : (
+									<>
+										{ php.version }
+									</>
+								)}
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">
+								SAPI
+							</th>
+							<td>
+								{ php.sapi }
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">
+								{ __( 'User', 'query-monitor' ) }
+							</th>
+							<td>
+								{ php.user || __( 'Unknown', 'query-monitor' ) }
+							</td>
+						</tr>
+						{Object.keys(php.variables).map(key =>
+							<tr key={key}>
+								<th scope="row">
+									{ key }
+								</th>
+								<td>
+									{ php.variables[ key ].after }
+								</td>
+							</tr>
+						)}
+						<tr>
+							<th scope="row">
+								{__( 'Error Reporting', 'query-monitor' )}
+							</th>
+							<td className="qm-has-toggle qm-ltr">
+								{ php.error_reporting }
+
+								<Toggler>
+									<ul className="qm-supplemental">
+										{Object.keys(php.error_levels).map(key =>
+											<li key={key}>
+												{ php.error_levels[ key ] ? (
+													<>
+														{key}&nbsp;&#x2713;
+													</>
+												):(
+													<span className="qm-false">
+														{key}
+													</span>
+												)}
+											</li>
+										)}
+									</ul>
+								</Toggler>
+							</td>
+						</tr>
+						{ php.extensions && (
+							<tr>
+								<th scope="row">
+									{__( 'Extensions', 'query-monitor' )}
+								</th>
+								<td className="qm-has-toggle qm-ltr">
+									{QM_i18n.number_format( Object.keys(php.extensions).length )}
+
+									<Toggler>
+										<ul className="qm-supplemental">
+											{Object.keys(php.extensions).map(key =>
+												<li key={key}>
+													{key} ({ php.extensions[ key ] || __( 'Unknown', 'query-monitor') })
+												</li>
+											)}
+										</ul>
+									</Toggler>
+								</td>
+							</tr>
+						)}
+					</tbody>
+				</table>
+			</section>
+		)
+	}
+
+}
+
+export default PHP;
