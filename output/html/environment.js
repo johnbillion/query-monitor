7a9f0dc52e5b8ffba40c4b612be9ced2a349561b
John Blackbourn <johnbillion@gmail.com>
2020-03-15 17:42:40 +0000

    Introduce the Server section.

--- output/html/environment.js
+++ output/html/environment.js
@@ -3,6 +3,7 @@ import NonTabular from '../non-tabular.js';
 import PHP from '../php.js';
 import DB from '../db.js';
 import WordPress from '../wordpress.js';
+import Server from '../server.js';
 import { __, _x, _n, sprintf } from '@wordpress/i18n';
 
 class Environment extends Component {
@@ -21,6 +22,7 @@ class Environment extends Component {
 					</>
 				)}
 				<WordPress wordpress={data.wp}/>
+				<Server server={data.server}/>
 			</NonTabular>
 		);
 	}
---
+++ output/server.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { __, _x, _n, sprintf } from '@wordpress/i18n';
+
+class WordPress extends Component {
+
+	render() {
+		const { server } = this.props;
+		const info = {
+			'name'    : __( 'Software', 'query-monitor' ),
+			'version' : __( 'Version', 'query-monitor' ),
+			'address' : __( 'Address', 'query-monitor' ),
+			'host'    : __( 'Host', 'query-monitor' ),
+			'OS'      : __( 'OS', 'query-monitor' ),
+		};
+
+		return (
+			<section>
+				<h3>
+					{__('Server','query-monitor')}
+				</h3>
+				<table>
+					<tbody>
+						{Object.keys(info).map(key =>
+							<tr key={key}>
+								<th scope="row">
+									{info[key]}
+								</th>
+								<td>
+									{server[key] || (
+										<span className="qm-warn">
+											<Warning/>
+											{__( 'Unknown', 'query-monitor' )}
+										</span>
+									)}
+								</td>
+							</tr>
+						)}
+					</tbody>
+				</table>
+			</section>
+		)
+	}
+
+}
+
+export default WordPress;
