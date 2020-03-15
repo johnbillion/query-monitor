95941d1224c25d09bb14d45940daf4df62965efc
John Blackbourn <johnbillion@gmail.com>
2020-03-15 11:26:05 +0000

    Introduce the Toggler component.

---
+++ output/toggler.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { __, _x, _n, sprintf } from '@wordpress/i18n';
+
+class Toggler extends Component {
+
+	render() {
+		return (
+			<>
+				<button
+					className="qm-toggle"
+					data-on="+"
+					data-off="-"
+					aria-expanded="false"
+					aria-label={__( 'Toggle more information', 'query-monitor' )}
+				>
+					<span aria-hidden="true">+</span>
+				</button>
+				<div className="qm-toggled">
+					{this.props.children}
+				</div>
+			</>
+		);
+	}
+
+}
+
+export default Toggler;
