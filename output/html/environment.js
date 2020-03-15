7864e1d96f9ec5943ec5fc8d0abc968199cbd81a
John Blackbourn <johnbillion@gmail.com>
2020-03-15 11:26:24 +0000

    Begin work on the Environment component.

---
+++ output/html/environment.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import NonTabular from '../non-tabular.js';
+import Warning from '../warning.js';
+import Toggler from '../toggler.js';
+import classnames from 'classnames';
+import { __, _x, _n, sprintf } from '@wordpress/i18n';
+
+class Environment extends Component {
+
+	render() {
+		const { data } = this.props;
+		const warning = data.php.old;
+		const classes = classnames( {
+			'qm-warn': warning,
+		} );
+
+		return (
+			<NonTabular id={this.props.id}>
+				<section>
+					<h3>{__('PHP','query-monitor')}</h3>
+					<table>
+						<tbody>
+							<tr className={ classes }>
+								<th scope="row">
+									{ __( 'Version', 'query-monitor' ) }
+								</th>
+								<td>
+									{ warning ? (
+										<>
+											<Warning/>
+											{ data.php.version }
+											&nbsp;(<a href="https://wordpress.org/support/update-php/" class="qm-external-link">
+												{__( 'Help', 'query-monitor' )}
+											</a>)
+										</>
+									) : (
+										<>
+											{ data.php.version }
+										</>
+									)}
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">
+									SAPI
+								</th>
+								<td>
+									{ data.php.sapi }
+								</td>
+							</tr>
+							<tr>
+								<th scope="row">
+									{ __( 'User', 'query-monitor' ) }
+								</th>
+								<td>
+									{ data.php.user || __( 'Unknown', 'query-monitor' ) }
+								</td>
+							</tr>
+							{Object.keys(data.php.variables).map(key =>
+								<tr key={key}>
+									<th scope="row">
+										{ key }
+									</th>
+									<td>
+										{ data.php.variables[ key ].after }
+									</td>
+								</tr>
+							)}
+							<tr>
+								<th scope="row">
+									{__( 'Error Reporting', 'query-monitor' )}
+								</th>
+								<td className="qm-has-toggle qm-ltr">
+									{ data.php.error_reporting }
+
+									<Toggler>
+										<ul className="qm-supplemental">
+											{Object.keys(data.php.error_levels).map(key =>
+												<li key={key}>
+													{ data.php.error_levels[ key ] ? (
+														<>
+															{key}&nbsp;&#x2713;
+														</>
+													):(
+														<span className="qm-false">
+															{key}
+														</span>
+													)}
+												</li>
+											)}
+										</ul>
+									</Toggler>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</section>
+			</NonTabular>
+		);
+	}
+
+}
+
+export default Environment;
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import DBDupes from '../output/html/db_dupes.js';
 import DBCallers from '../output/html/db_callers.js';
 import DBComponents from '../output/html/db_components.js';
 import BlockEditor from '../output/html/block_editor.js';
+import Environment from '../output/html/environment.js';
 
 jQuery(function($) {
 	ReactDOM.render(<Conditionals data={qm_data.conditionals.data} enabled={qm_data.conditionals.enabled} id="conditionals" /> , document.getElementById('qm-conditionals-container'));
@@ -27,6 +28,6 @@ jQuery(function($) {
 	ReactDOM.render(<DBDupes data={qm_data.db_dupes.data} enabled={qm_data.db_dupes.enabled} id="db_dupes" /> , document.getElementById('qm-db_dupes-container'));
 	ReactDOM.render(<DBCallers data={qm_data.db_callers.data} enabled={qm_data.db_callers.enabled} id="db_callers" /> , document.getElementById('qm-db_callers-container'));
 	ReactDOM.render(<DBComponents data={qm_data.db_components.data} enabled={qm_data.db_components.enabled} id="db_components" /> , document.getElementById('qm-db_components-container'));
-
 	ReactDOM.render(<BlockEditor data={qm_data.block_editor.data} enabled={qm_data.block_editor.enabled} id="block_editor" /> , document.getElementById('qm-block_editor-container'));
+	ReactDOM.render(<Environment data={qm_data.environment.data} enabled={qm_data.environment.enabled} id="environment" /> , document.getElementById('qm-environment-container'));
 } );
