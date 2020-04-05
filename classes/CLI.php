9551b4725a90007e3473f7c1ce0868f0495fb1ec
John Blackbourn <johnbillion@gmail.com>
2020-04-06 00:06:55 +0100

    Switch to creating an absolute symlink on the CLI.
    This reduces complexity and brings behavioural consistency with the activation hook.

--- classes/CLI.php
+++ classes/CLI.php
@@ -30,53 +30,12 @@ class QM_CLI extends QM_Plugin {
 		if ( file_exists( $drop_in ) ) {
 			WP_CLI::error( 'Unknown wp-content/db.php already exists.' );
 		}
-		$db_file = $this->plugin_path( 'wp-content/db.php' );
-		$target       = self::get_relative_path( $drop_in, $db_file );
-		chdir( WP_CONTENT_DIR );
-		// @codingStandardsIgnoreStart
-		if ( symlink( $target, 'db.php' ) ) {
-			// @codingStandardsIgnoreEnd
+
+		if ( symlink( $this->plugin_path( 'wp-content/db.php' ), $drop_in ) ) {
 			WP_CLI::success( 'Enabled QM Extended query information by creating wp-content/db.php symlink.' );
 		} else {
 			WP_CLI::error( 'Failed create wp-content/db.php symlink and enable QM Extended query information.' );
 		}
-	}
-
-	/**
-	 * Get the relative path between two files
-	 *
-	 * @see http://stackoverflow.com/questions/2637945/getting-relative-path-from-absolute-path-in-php
-	 */
-	private static function get_relative_path( $from, $to ) {
-		// some compatibility fixes for Windows paths
-		$from = is_dir( $from ) ? rtrim( $from, '\/' ) . '/' : $from;
-		$to   = is_dir( $to ) ? rtrim( $to, '\/' ) . '/' : $to;
-		$from = str_replace( '\\', '/', $from );
-		$to   = str_replace( '\\', '/', $to );
-
-		$from     = explode( '/', $from );
-		$to       = explode( '/', $to );
-		$rel_path = $to;
-
-		foreach ( $from as $depth => $dir ) {
-			// find first non-matching dir
-			if ( $dir === $to[ $depth ] ) {
-				// ignore this directory
-				array_shift( $rel_path );
-			} else {
-				// get number of remaining dirs to $from
-				$remaining = count( $from ) - $depth;
-				if ( $remaining > 1 ) {
-					// add traversals up to first matching dir
-					$pad_length = ( count( $rel_path ) + $remaining - 1 ) * -1;
-					$rel_path   = array_pad( $rel_path, $pad_length, '..' );
-					break;
-				} else {
-					$rel_path[0] = './' . $rel_path[0];
-				}
-			}
-		}
-		return implode( '/', $rel_path );
 	}
 
 	public static function init( $file = null ) {
