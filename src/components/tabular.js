5f8496918d09ecefdbbaf0e5417b9bde1246e5b6
John Blackbourn <johnbillion@gmail.com>
2020-05-16 14:14:40 +0100

    Introduce the base components for panels.

---
+++ src/components/tabular.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export class Tabular extends Component {
+
+	render() {
+		const caption = `qm-${this.props.id}-caption`;
+		return (
+			<div className="qm" id={`qm-${this.props.id}`} role="tabpanel" aria-labelledby={caption} tabIndex="-1">
+				<table className="qm-sortable">
+					<caption className="qm-screen-reader-text">
+						<h2 id={caption}>
+							@TODO
+						</h2>
+					</caption>
+					{this.props.children}
+				</table>
+			</div>
+		);
+	}
+
+}
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export { Tabular } from './components/tabular';
+export { NonTabular } from './components/non-tabular';
