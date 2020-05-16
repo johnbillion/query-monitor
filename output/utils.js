0914466643757cdd11c08cd0d235286edaf642fe
John Blackbourn <johnbillion@gmail.com>
2020-05-16 14:18:14 +0100

    Switch to using the external QMI components.

--- output/utils.js
+++ output/utils.js
@@ -1,10 +1,13 @@
 
+export {
+	NonTabular,
+	Tabular,
+} from 'qmi';
+
 export { Caller } from './caller';
 export { Frame } from './frame';
-export { NonTabular } from './non-tabular';
 export { Notice } from './notice';
 export { PanelFooter } from './panel-footer';
 export { QMComponent } from './component';
-export { Tabular } from './tabular';
 export { Toggler } from './toggler';
 export { Warning } from './warning';
