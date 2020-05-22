4e91d8a228a979869e4e2ae656b2d8a73166d310
John Blackbourn <johnbillion@gmail.com>
2020-05-22 23:10:18 +0100

    Get the bell on.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack');
 const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const BellOnBundlerErrorPlugin = require( 'bell-on-bundler-error-plugin' );
 
 module.exports = {
   ...defaultConfig,
   plugins: [
+    new BellOnBundlerErrorPlugin(),
     new webpack.ProvidePlugin({
       react: 'react',
     }),
