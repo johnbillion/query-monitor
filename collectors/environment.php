bddc44793d3c5a9931ef78e542cb4a42d3feb968
Morgan Estes <morganestes@users.noreply.github.com>
2020-07-10 12:12:25 -0500

    Fix PHPCS extra line error
    Remove the extra line so PHPCS will be happy.

--- collectors/environment.php
+++ collectors/environment.php
@@ -262,7 +262,6 @@ class QM_Collector_Environment extends QM_Collector {
 		if ( function_exists( 'posix_getpwuid' ) ) {
 			$u     = posix_getpwuid( posix_getuid() );
 			$g     = posix_getgrgid( $u['gid'] );
-			
 			if ( ! empty( $u ) && ! empty( $g ) ) {
 				$php_u = $u['name'] . ':' . $g['name'];
 			}
