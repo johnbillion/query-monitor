37405d93df86c51ee9bd3a2ae94df90e2bbab1ce
John Blackbourn <johnbillion@gmail.com>
2020-08-01 14:54:04 +0100

    Don't need jQuery for this.

--- src/index.tsx
+++ src/index.tsx
@@ -42,7 +42,7 @@ declare var qm_data: {
 	logger?: QMPanelData;
 };
 
-jQuery(function($) {
+document.addEventListener('DOMContentLoaded',function() {
 	ReactDOM.render(<Conditionals data={qm_data.conditionals.data} enabled={qm_data.conditionals.enabled} id="conditionals" /> , document.getElementById('qm-conditionals-container'));
 	ReactDOM.render(<Caps data={qm_data.caps.data} enabled={qm_data.caps.enabled} id="caps" /> , document.getElementById('qm-caps-container'));
 	ReactDOM.render(<Transients data={qm_data.transients.data} enabled={qm_data.transients.enabled} id="transients" /> , document.getElementById('qm-transients-container'));
