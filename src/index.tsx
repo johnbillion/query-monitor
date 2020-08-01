6bbe02868f24903b77d3b6fe29676599e1f38653
John Blackbourn <johnbillion@gmail.com>
2020-08-01 14:57:18 +0100

    Format for readability.

--- src/index.tsx
+++ src/index.tsx
@@ -43,26 +43,136 @@ declare var qm_data: {
 };
 
 document.addEventListener('DOMContentLoaded',function() {
-	ReactDOM.render(<Conditionals data={qm_data.conditionals.data} enabled={qm_data.conditionals.enabled} id="conditionals" /> , document.getElementById('qm-conditionals-container'));
-	ReactDOM.render(<Caps data={qm_data.caps.data} enabled={qm_data.caps.enabled} id="caps" /> , document.getElementById('qm-caps-container'));
-	ReactDOM.render(<Transients data={qm_data.transients.data} enabled={qm_data.transients.enabled} id="transients" /> , document.getElementById('qm-transients-container'));
-	ReactDOM.render(<Languages data={qm_data.languages.data} enabled={qm_data.languages.enabled} id="languages" /> , document.getElementById('qm-languages-container'));
+	ReactDOM.render(
+		<Conditionals
+			data={qm_data.conditionals.data}
+			enabled={qm_data.conditionals.enabled}
+			id="conditionals"
+		/>,
+		document.getElementById('qm-conditionals-container')
+	);
+	ReactDOM.render(
+		<Caps
+			data={qm_data.caps.data}
+			enabled={qm_data.caps.enabled}
+			id="caps"
+		/>,
+		document.getElementById('qm-caps-container')
+	);
+	ReactDOM.render(
+		<Transients
+			data={qm_data.transients.data}
+			enabled={qm_data.transients.enabled}
+			id="transients"
+		/>,
+		document.getElementById('qm-transients-container')
+	);
+	ReactDOM.render(
+		<Languages
+			data={qm_data.languages.data}
+			enabled={qm_data.languages.enabled}
+			id="languages"
+		/>,
+		document.getElementById('qm-languages-container')
+	);
 
 	Object.keys(qm_data.db_queries.data.dbs).map(function(key){
-		let data = qm_data.db_queries.data.dbs[key];
-		ReactDOM.render(<DBQueries data={data} enabled={qm_data.db_queries.enabled} id="db_queries-wpdb" /> , document.getElementById('qm-db_queries-container'));
+		ReactDOM.render(
+			<DBQueries
+				data={qm_data.db_queries.data.dbs[key]}
+				enabled={qm_data.db_queries.enabled}
+				id="db_queries-wpdb"
+			/>,
+			document.getElementById('qm-db_queries-container')
+		);
 	});
 
-	ReactDOM.render(<DBDupes data={qm_data.db_dupes.data} enabled={qm_data.db_dupes.enabled} id="db_dupes" /> , document.getElementById('qm-db_dupes-container'));
-	ReactDOM.render(<DBCallers data={qm_data.db_callers.data} enabled={qm_data.db_callers.enabled} id="db_callers" /> , document.getElementById('qm-db_callers-container'));
-	ReactDOM.render(<DBComponents data={qm_data.db_components.data} enabled={qm_data.db_components.enabled} id="db_components" /> , document.getElementById('qm-db_components-container'));
-	ReactDOM.render(<BlockEditor data={qm_data.block_editor.data} enabled={qm_data.block_editor.enabled} id="block_editor" /> , document.getElementById('qm-block_editor-container'));
-	ReactDOM.render(<Environment data={qm_data.environment.data} enabled={qm_data.environment.enabled} id="environment" /> , document.getElementById('qm-environment-container'));
-	ReactDOM.render(<Scripts data={qm_data.assets_scripts.data} enabled={qm_data.assets_scripts.enabled} id="assets_scripts" /> , document.getElementById('qm-assets_scripts-container'));
-	ReactDOM.render(<Styles data={qm_data.assets_styles.data} enabled={qm_data.assets_styles.enabled} id="assets_styles" /> , document.getElementById('qm-assets_styles-container'));
-	qm_data.hooks && ReactDOM.render(<Hooks data={qm_data.hooks.data} enabled={qm_data.hooks.enabled} id="hooks" /> , document.getElementById('qm-hooks-container'));
-	qm_data.admin && ReactDOM.render(<Admin data={qm_data.admin.data} enabled={qm_data.admin.enabled} id="admin" /> , document.getElementById('qm-admin-container'));
-	qm_data.http && ReactDOM.render(<HTTP data={qm_data.http.data} enabled={qm_data.http.enabled} id="http" /> , document.getElementById('qm-http-container'));
-	qm_data.logger && ReactDOM.render(<Logger data={qm_data.logger.data} enabled={qm_data.logger.enabled} id="logger" /> , document.getElementById('qm-logger-container'));
-
+	ReactDOM.render(
+		<DBDupes
+			data={qm_data.db_dupes.data}
+			enabled={qm_data.db_dupes.enabled}
+			id="db_dupes"
+		/>,
+		document.getElementById('qm-db_dupes-container')
+	);
+	ReactDOM.render(
+		<DBCallers
+			data={qm_data.db_callers.data}
+			enabled={qm_data.db_callers.enabled}
+			id="db_callers"
+		/>,
+		document.getElementById('qm-db_callers-container')
+	);
+	ReactDOM.render(
+		<DBComponents
+			data={qm_data.db_components.data}
+			enabled={qm_data.db_components.enabled}
+			id="db_components"
+		/>,
+		document.getElementById('qm-db_components-container')
+	);
+	ReactDOM.render(
+		<BlockEditor
+			data={qm_data.block_editor.data}
+			enabled={qm_data.block_editor.enabled}
+			id="block_editor"
+		/>,
+		document.getElementById('qm-block_editor-container')
+	);
+	ReactDOM.render(
+		<Environment
+			data={qm_data.environment.data}
+			enabled={qm_data.environment.enabled}
+			id="environment"
+		/>,
+		document.getElementById('qm-environment-container')
+	);
+	ReactDOM.render(
+		<Scripts
+			data={qm_data.assets_scripts.data}
+			enabled={qm_data.assets_scripts.enabled}
+			id="assets_scripts"
+		/>,
+		document.getElementById('qm-assets_scripts-container')
+	);
+	ReactDOM.render(
+		<Styles
+			data={qm_data.assets_styles.data}
+			enabled={qm_data.assets_styles.enabled}
+			id="assets_styles"
+		/>,
+		document.getElementById('qm-assets_styles-container')
+	);
+	ReactDOM.render(
+		<Hooks
+			data={qm_data.hooks.data}
+			enabled={qm_data.hooks.enabled}
+			id="hooks"
+		/>,
+		document.getElementById('qm-hooks-container')
+	);
+	qm_data.admin && ReactDOM.render(
+		<Admin
+			data={qm_data.admin.data}
+			enabled={qm_data.admin.enabled}
+			id="admin"
+		/>,
+		document.getElementById('qm-admin-container')
+	);
+	ReactDOM.render(
+		<HTTP
+			data={qm_data.http.data}
+			enabled={qm_data.http.enabled}
+			id="http"
+		/>,
+		document.getElementById('qm-http-container')
+	);
+	qm_data.logger && ReactDOM.render(
+		<Logger
+			data={qm_data.logger.data}
+			enabled={qm_data.logger.enabled}
+			id="logger"
+		/>,
+		document.getElementById('qm-logger-container')
+	);
 } );
