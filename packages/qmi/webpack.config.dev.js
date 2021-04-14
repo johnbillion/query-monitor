dee4b9a79e92be489a77cadf7f4c33393aae376c
John Blackbourn <johnbillion@gmail.com>
2021-04-14 19:07:59 +0100

    More tabs.

--- packages/qmi/webpack.config.dev.js
+++ packages/qmi/webpack.config.dev.js
@@ -2,42 +2,42 @@ const path = require( 'path' );
 const BellOnBundlerErrorPlugin = require( 'bell-on-bundler-error-plugin' );
 
 module.exports = {
-  mode: 'development',
-  watch: true,
-  resolve: {
-    extensions: [
-      '.ts',
-      '.tsx',
-      '.js',
-      '.jsx',
-      '.json',
-    ]
-  },
-  plugins: [
-    new BellOnBundlerErrorPlugin(),
-  ],
-  entry: './index.ts',
-  output: {
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    path: path.resolve( 'build' ),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-    ],
-  },
-  externals: {
-    react: 'react',
-    reactDOM: 'reactDOM',
-  },
+	mode: 'development',
+	watch: true,
+	resolve: {
+		extensions: [
+			'.ts',
+			'.tsx',
+			'.js',
+			'.jsx',
+			'.json',
+		]
+	},
+	plugins: [
+		new BellOnBundlerErrorPlugin(),
+	],
+	entry: './index.ts',
+	output: {
+		filename: 'index.js',
+		libraryTarget: 'umd',
+		path: path.resolve( 'build' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				loader: 'source-map-loader',
+			},
+		],
+	},
+	externals: {
+		react: 'react',
+		reactDOM: 'reactDOM',
+	},
 };
--- packages/qmi/webpack.config.prod.js
+++ packages/qmi/webpack.config.prod.js
@@ -2,41 +2,41 @@ const path = require( 'path' );
 const BellOnBundlerErrorPlugin = require( 'bell-on-bundler-error-plugin' );
 
 module.exports = {
-  mode: 'production',
-  resolve: {
-    extensions: [
-      '.ts',
-      '.tsx',
-      '.js',
-      '.jsx',
-      '.json',
-    ]
-  },
-  plugins: [
-    new BellOnBundlerErrorPlugin(),
-  ],
-  entry: './index.ts',
-  output: {
-    filename: 'index.js',
-    libraryTarget: 'commonjs2',
-    path: path.resolve( 'build' ),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts(x?)$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
-      },
-    ],
-  },
-  externals: {
-    react: 'react',
-    reactDOM: 'reactDOM',
-  },
+	mode: 'production',
+	resolve: {
+		extensions: [
+			'.ts',
+			'.tsx',
+			'.js',
+			'.jsx',
+			'.json',
+		]
+	},
+	plugins: [
+		new BellOnBundlerErrorPlugin(),
+	],
+	entry: './index.ts',
+	output: {
+		filename: 'index.js',
+		libraryTarget: 'commonjs2',
+		path: path.resolve( 'build' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				loader: 'ts-loader',
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				loader: 'source-map-loader',
+			},
+		],
+	},
+	externals: {
+		react: 'react',
+		reactDOM: 'reactDOM',
+	},
 };
