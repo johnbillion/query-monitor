187a170cd1a9003138b3db66b8b1e9ec12a3e865
John Blackbourn <johnbillion@gmail.com>
2021-04-15 00:35:58 +0100

    Tidying up.

--- packages/qmi/src/non-tabular.tsx
+++ packages/qmi/src/non-tabular.tsx
@@ -7,10 +7,17 @@ export interface NonTabularProps {
 export class NonTabular extends React.Component<NonTabularProps, Record<string, unknown>> {
 
 	render() {
-		const caption = `qm-${this.props.id}-caption`;
+		const caption = `qm-${ this.props.id }-caption`;
+
 		return (
-			<div className="qm-panel-container" id={ `qm-${this.props.id}-container` }>
-				<div aria-labelledby={ caption } className="qm qm-non-tabular" id={ `qm-${this.props.id}` } role="tabpanel" tabIndex={ -1 }>
+			<div className="qm-panel-container" id={ `qm-${ this.props.id }-container` }>
+				<div
+					aria-labelledby={ caption }
+					className="qm qm-non-tabular"
+					id={ `qm-${ this.props.id }` }
+					role="tabpanel"
+					tabIndex={ -1 }
+				>
 					<div className="qm-boxed">
 						<h2 className="qm-screen-reader-text" id={ caption }>
 							@TODO
