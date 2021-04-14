317c1b797e0a1990b9d1e68f3d269e347fa09317
John Blackbourn <johnbillion@gmail.com>
2021-04-14 18:49:19 +0100

    Tabs!

--- webpack.config.js
+++ webpack.config.js
@@ -6,46 +6,46 @@ const BellOnBundlerErrorPlugin = require( 'bell-on-bundler-error-plugin' );
 
 /** @type {webpack.Configuration} */
 module.exports = {
-  ...defaultConfig,
-  entry: './src/index.tsx',
-  resolve: {
-    extensions: [
-      '.ts',
-      '.tsx',
-      '.js',
-      '.jsx',
-      '.json',
-    ]
-  },
-  module: {
-    noParse: [
-      /tests/,
-      /vendor/,
-    ],
-    rules: [
-      ...defaultConfig.module.rules,
-      {
-        test: /\.ts(x?)$/,
-        exclude: [
-          /node_modules/,
-        ],
-        loader: 'ts-loader',
-      },
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        exclude: [
-          /node_modules/,
-        ],
-        loader: 'source-map-loader',
-      },
-    ],
-  },
-  plugins: [
-    ...defaultConfig.plugins,
-    new BellOnBundlerErrorPlugin(),
-    new webpack.ProvidePlugin({
-      react: 'react',
-    }),
-  ],
+	...defaultConfig,
+	entry: './src/index.tsx',
+	resolve: {
+		extensions: [
+			'.ts',
+			'.tsx',
+			'.js',
+			'.jsx',
+			'.json',
+		]
+	},
+	module: {
+		noParse: [
+			/tests/,
+			/vendor/,
+		],
+		rules: [
+			...defaultConfig.module.rules,
+			{
+				test: /\.ts(x?)$/,
+				exclude: [
+					/node_modules/,
+				],
+				loader: 'ts-loader',
+			},
+			{
+				enforce: 'pre',
+				test: /\.js$/,
+				exclude: [
+					/node_modules/,
+				],
+				loader: 'source-map-loader',
+			},
+		],
+	},
+	plugins: [
+		...defaultConfig.plugins,
+		new BellOnBundlerErrorPlugin(),
+		new webpack.ProvidePlugin({
+			react: 'react',
+		}),
+	],
 };
