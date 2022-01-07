113e52a3367b1e20038c8d0c014efa7544381e9b
John Blackbourn <johnbillion@gmail.com>
2022-01-07 21:41:20 +0100

    Version 3.8.2.

--- query-monitor.php
+++ query-monitor.php
@@ -10,7 +10,7 @@
  *
  * Plugin Name:  Query Monitor
  * Description:  The Developer Tools Panel for WordPress.
- * Version:      3.8.1
+ * Version:      3.8.2
  * Plugin URI:   https://querymonitor.com/
  * Author:       John Blackbourn
  * Author URI:   https://querymonitor.com/
@@ -33,7 +33,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'QM_VERSION', '3.8.1' );
+define( 'QM_VERSION', '3.8.2' );
 
 $qm_dir = dirname( __FILE__ );
 
