a863a41b3d035147b2f1ea292da2d9ede1794866
John Blackbourn <johnbillion@gmail.com>
2022-01-08 23:35:24 +0100

    Cover all roles with the basic access test.

--- tests/acceptance/BasicCest.php
+++ tests/acceptance/BasicCest.php
@@ -3,8 +3,51 @@
  * Acceptance tests for basic user access.
  */
 
+use Codeception\Example;
+
 class BasicCest {
-	public function BasicAccessShouldWorkAsExpected( AcceptanceTester $I ) {
-		$I->loginAsAdmin();
+	/**
+	 * @dataProvider dataRoles
+	 */
+	public function BasicAccessShouldWorkAsExpected( AcceptanceTester $I, Example $data ): void {
+		$I->haveUserInDatabase( $data['role'], $data['role'] );
+		$I->loginAs( $data['role'], $data['role'] );
+		$I->see(
+			$data['role'],
+			'#wpadminbar .display-name'
+		);
+
+		if ( $data['access'] ) {
+			$I->seeElement( '#wp-admin-bar-query-monitor' );
+			$I->seeElementInDOM( '#query-monitor-main');
+		} else {
+			$I->dontSeeElement( '#wp-admin-bar-query-monitor' );
+			$I->dontSeeElementInDOM( '#query-monitor-main');
+		}
+	}
+
+	private function dataRoles(): array {
+		return [
+			'administrator' => [
+				'role' => 'administrator',
+				'access' => true,
+			],
+			'editor' => [
+				'role' => 'editor',
+				'access' => false,
+			],
+			'author' => [
+				'role' => 'author',
+				'access' => false,
+			],
+			'contributor' => [
+				'role' => 'contributor',
+				'access' => false,
+			],
+			'subscriber' => [
+				'role' => 'subscriber',
+				'access' => false,
+			],
+		];
 	}
 }
