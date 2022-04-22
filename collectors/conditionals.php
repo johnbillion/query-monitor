9401d908c78855f3671d18ac6ac0fc17d884d235
John Blackbourn <johnbillion@gmail.com>
2022-04-22 22:30:37 +0200

    Switch to a DTO for the template conditionals collector.

--- collectors/conditionals.php
+++ collectors/conditionals.php
@@ -9,9 +9,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-class QM_Collector_Conditionals extends QM_Collector {
+/**
+ * @extends QM_DataCollector<QM_Data_Conditionals>
+ */
+class QM_Collector_Conditionals extends QM_DataCollector {
 
 	public $id = 'conditionals';
+
+	public function get_storage() {
+		return new QM_Data_Conditionals();
+	}
 
 	/**
 	 * @return void
@@ -100,7 +107,7 @@ class QM_Collector_Conditionals extends QM_Collector {
 				$na[] = $cond;
 			}
 		}
-		$this->data['conds'] = compact( 'true', 'false', 'na' );
+		$this->data->conds = compact( 'true', 'false', 'na' );
 
 	}
 
