dd4f228843964afd747a12e9fcfc72d87b834539
John Blackbourn <johnbillion@gmail.com>
2022-05-03 16:30:24 +0200

    None of these numeric values should be localised.

--- output/raw/cache.php
+++ output/raw/cache.php
@@ -33,9 +33,9 @@ class QM_Output_Raw_Cache extends QM_Output_Raw {
 		$data = $this->collector->get_data();
 
 		if ( isset( $data['stats'] ) && isset( $data['cache_hit_percentage'] ) ) {
-			$output['hit_percentage'] = (float) number_format_i18n( $data['cache_hit_percentage'], 1 );
-			$output['hits'] = (int) number_format_i18n( $data['stats']['cache_hits'], 0 );
-			$output['misses'] = (int) number_format_i18n( $data['stats']['cache_misses'], 0 );
+			$output['hit_percentage'] = round( $data['cache_hit_percentage'], 1 );
+			$output['hits'] = (int) $data['stats']['cache_hits'];
+			$output['misses'] = (int) $data['stats']['cache_misses'];
 		}
 
 		return $output;
