dfe037bdb21dacf235dc82cf0f1de56d21d17ff2
John Blackbourn <johnbillion@gmail.com>
2022-05-12 19:25:19 +0200

    None of this is needed any more.

--- bin/integration-tests.sh
+++ bin/integration-tests.sh
@@ -7,15 +7,10 @@ set -eo pipefail
 # Run the integration tests:
 echo "Running tests..."
 
-# Why are these sent to /dev/null? See https://github.com/docker/compose/issues/8833
 docker-compose exec \
-	-T \
 	--workdir /var/www/html/wp-content/plugins/query-monitor php \
-	./vendor/bin/codecept run integration --env singlesite --skip-group ms-required "$1" \
-	< /dev/null
+	./vendor/bin/codecept run integration --env singlesite --skip-group ms-required "$1"
 
 docker-compose exec \
-	-T \
 	--workdir /var/www/html/wp-content/plugins/query-monitor php \
-	./vendor/bin/codecept run integration --env multisite --skip-group ms-excluded "$1" \
-	< /dev/null
+	./vendor/bin/codecept run integration --env multisite --skip-group ms-excluded "$1"
