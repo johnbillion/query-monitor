2d3481d82e3e7e568a5470aafe52bb93e6808590
John Blackbourn <johnbillion@gmail.com>
2022-09-09 02:10:12 +0100

    Don't need this any more.

--- classes/QM_DB.php
+++ classes/QM_DB.php
@@ -77,12 +77,6 @@ class QM_DB extends wpdb {
 				$code = mysqli_errno( $this->dbh );
 			}
 
-			if ( is_resource( $this->dbh ) ) {
-				// Please do not report this code as a PHP 7 incompatibility. Observe the surrounding logic.
-				// phpcs:ignore
-				$code = mysql_errno( $this->dbh );
-			}
-
 			$this->queries[ $i ]['result'] = new WP_Error( $code, $this->last_error );
 		} else {
 			$this->queries[ $i ]['result'] = $result;
