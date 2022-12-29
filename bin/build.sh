3a2e66dcf82b3fa7942c89e781a444b16b72e76b
John Blackbourn <johnbillion@gmail.com>
2022-12-29 21:29:54 +0000

    Remove autoloading for `\Composer\InstalledVersions`. See #696.

--- bin/build.sh
+++ bin/build.sh
@@ -11,3 +11,7 @@ composer dump-autoload --no-dev
 # Wrap the call to `setClassMapAuthoritative` in a `method_exists` check:
 sed -i.bak 's/\$loader->setClassMapAuthoritative(true);/if (method_exists(\$loader,"setClassMapAuthoritative")){\n            \$loader->setClassMapAuthoritative(true);\n        }/' vendor/composer/autoload_real.php
 rm vendor/composer/autoload_real.php.bak
+
+# Remove autoloading for `\Composer\InstalledVersions`:
+sed -i.bak '/Composer\\\\InstalledVersions/d' vendor/composer/autoload_classmap.php
+rm vendor/composer/autoload_classmap.php.bak
