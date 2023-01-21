5711b6aac546f9ab5c8d02e95f728665075c921c
Viktor Szépe <viktor@szepe.net>
2023-01-21 03:21:34 +0000

    Remove debugging from CI

--- bin/acceptance-tests.sh
+++ bin/acceptance-tests.sh
@@ -8,48 +8,48 @@ echo "Starting up..."
 
 # Prep:
 docker-compose --profile acceptance-tests up -d
-WP_PORT=`docker inspect --type=container --format='{{(index .NetworkSettings.Ports "80/tcp" 0).HostPort}}' qm-server`
-CHROME_PORT=`docker inspect --type=container --format='{{(index .NetworkSettings.Ports "4444/tcp" 0).HostPort}}' qm-chrome`
-DATABASE_PORT=`docker inspect --type=container --format='{{(index .NetworkSettings.Ports "3306/tcp" 0).HostPort}}' qm-database`
+WP_PORT="$(docker inspect --type=container --format='{{(index .NetworkSettings.Ports "80/tcp" 0).HostPort}}' qm-server)"
+CHROME_PORT="$(docker inspect --type=container --format='{{(index .NetworkSettings.Ports "4444/tcp" 0).HostPort}}' qm-chrome)"
+DATABASE_PORT="$(docker inspect --type=container --format='{{(index .NetworkSettings.Ports "3306/tcp" 0).HostPort}}' qm-database)"
 WP_URL="http://host.docker.internal:${WP_PORT}"
 WP="docker-compose run --rm wpcli --url=${WP_URL}"
 
 # Wait for the web server:
-./node_modules/.bin/wait-port -t 10000 $WP_PORT
+./node_modules/.bin/wait-port -t 10000 "${WP_PORT}"
 
 # Wait for MariaDB:
-while ! docker-compose exec -T database /bin/bash -c 'mysqladmin ping --user="${MYSQL_USER}" --password="${MYSQL_PASSWORD}" --silent' | grep 'mysqld is alive' >/dev/null; do
+while ! docker-compose exec -T database /bin/bash -c 'mysqladmin ping --user="${MYSQL_USER}" --password="${MYSQL_PASSWORD}" --silent' | grep --quiet 'mysqld is alive'; do
 	echo 'Waiting for MariaDB...'
 	sleep 1
 done
 
 # Wait for Selenium:
-while ! curl -sSL "http://localhost:${CHROME_PORT}/wd/hub/status" 2>&1 | grep '"ready": true' >/dev/null; do
+while ! curl -sSL "http://localhost:${CHROME_PORT}/wd/hub/status" 2>&1 | grep --quiet '"ready": true'; do
 	echo 'Waiting for Selenium...'
 	sleep 1
 done
 
 # Reset or install the test database:
 echo "Installing database..."
-$WP db reset --yes
+"${WP}" db reset --yes
 
 # Install WordPress:
 echo "Installing WordPress..."
-$WP core install \
+"${WP}" core install \
 	--title="Example" \
 	--admin_user="admin" \
 	--admin_password="admin" \
 	--admin_email="admin@example.com" \
 	--skip-email \
 	--require="wp-content/plugins/query-monitor/bin/mysqli_report.php"
-echo "Home URL: $WP_URL"
-$WP plugin activate query-monitor
+echo "Home URL: ${WP_URL}"
+"${WP}" plugin activate query-monitor
 
 # Run the acceptance tests:
 echo "Running tests..."
-TEST_SITE_WEBDRIVER_PORT=$CHROME_PORT \
-	TEST_SITE_DATABASE_PORT=$DATABASE_PORT \
-	TEST_SITE_WP_URL=$WP_URL \
+TEST_SITE_WEBDRIVER_PORT="${CHROME_PORT}" \
+	TEST_SITE_DATABASE_PORT="${DATABASE_PORT}" \
+	TEST_SITE_WP_URL="${WP_URL}" \
 	./vendor/bin/codecept run acceptance --steps "$1"
 
 # Ciao:
--- bin/integration-tests.sh
+++ bin/integration-tests.sh
@@ -7,7 +7,7 @@ set -eo pipefail
 echo "Starting up..."
 
 # Wait for MariaDB:
-while ! docker-compose exec -T database /bin/bash -c 'mysqladmin ping --user="${MYSQL_USER}" --password="${MYSQL_PASSWORD}" --silent' | grep 'mysqld is alive' >/dev/null; do
+while ! docker-compose exec -T database /bin/bash -c 'mysqladmin ping --user="${MYSQL_USER}" --password="${MYSQL_PASSWORD}" --silent' | grep --quiet 'mysqld is alive'; do
 	echo 'Waiting for MariaDB...'
 	sleep 1
 done
@@ -15,15 +15,15 @@ done
 # Run the integration tests:
 echo "Running tests..."
 
-# Why are these sent to /dev/null? See https://github.com/docker/compose/issues/8833
+# Why is /dev/null piped into stdout? See https://github.com/docker/compose/issues/8833
 docker-compose exec \
-	-T \
+	--no-TTY \
 	--workdir /var/www/html/wp-content/plugins/query-monitor php \
-	./vendor/bin/codecept run integration --env singlesite --skip-group ms-required --debug "$1" \
+	./vendor/bin/codecept run integration --env singlesite --skip-group ms-required "$1" \
 	< /dev/null
 
 docker-compose exec \
-	-T \
+	--no-TTY \
 	--workdir /var/www/html/wp-content/plugins/query-monitor php \
-	./vendor/bin/codecept run integration --env multisite --skip-group ms-excluded --debug "$1" \
+	./vendor/bin/codecept run integration --env multisite --skip-group ms-excluded "$1" \
 	< /dev/null
