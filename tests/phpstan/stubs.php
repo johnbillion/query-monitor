6e59fcc342a4cebb4ef8b4a6029c583c190a191a
John Blackbourn <johnbillion@gmail.com>
2023-03-19 00:05:55 +0100

    Use PHPStan stubs for core from plugin-infrastructure.

--- tests/phpstan/stubs.php
+++ tests/phpstan/stubs.php
@@ -7,24 +7,6 @@ define( 'QM_DB_EXPENSIVE', 1 );
 define( 'QM_EDITOR_COOKIE', '' );
 define( 'QM_ERROR_FATALS', 1 );
 
-// WP core constants:
-
-define( 'DB_HOST', '' );
-define( 'DB_NAME', '' );
-define( 'DB_PASSWORD', '' );
-define( 'DB_USER', '' );
-
-define( 'SAVEQUERIES', false );
-define( 'WP_CONTENT_DIR', '' );
-define( 'WP_MEMORY_LIMIT', '' );
-define( 'WP_PLUGIN_DIR', '' );
-define( 'WPINC', '' );
-define( 'WP_DEFAULT_THEME', '' );
-
-define( 'COOKIE_DOMAIN', '' );
-define( 'COOKIEHASH', '' );
-define( 'COOKIEPATH', '' );
-
 // Third party constants:
 
 define( 'Altis\ROOT_DIR', '' );
