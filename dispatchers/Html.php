a73bdc6d1d3355f27ed68586eccaf4ade5a266d1
Caleb Stauffer <develop+github@calebstauffer.com>
2023-06-16 10:54:53 -0400

    add nova editor

--- dispatchers/Html.php
+++ dispatchers/Html.php
@@ -570,6 +570,7 @@ class QM_Dispatcher_Html extends QM_Dispatcher {
 			'Default/Xdebug' => '',
 			'Atom' => 'atom',
 			'Netbeans' => 'netbeans',
+			'Nova' => 'nova',
 			'PhpStorm' => 'phpstorm',
 			'Sublime Text' => 'sublime',
 			'TextMate' => 'textmate',
