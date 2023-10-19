c0e1b840b57702c5c4cd0645e455eb7c08c03608
John Blackbourn <johnbillion@gmail.com>
2023-10-19 18:24:45 +0200

    This might not be set.

--- collectors/db_callers.php
+++ collectors/db_callers.php
@@ -31,8 +31,10 @@ class QM_Collector_DB_Callers extends QM_DataCollector {
 			/** @var QM_Data_DB_Queries $dbq_data */
 			$dbq_data = $dbq->get_data();
 
-			$this->data->times = $dbq_data->times;
-			QM_Util::rsort( $this->data->times, 'ltime' );
+			if ( isset( $dbq_data->times ) ) {
+				$this->data->times = $dbq_data->times;
+				QM_Util::rsort( $this->data->times, 'ltime' );
+			}
 
 			$this->data->types = $dbq_data->types;
 		}
