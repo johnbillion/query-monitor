9e8d58e7d0eb6aa45774e49b6f48a37a5d96e39b
John Blackbourn <johnbillion@gmail.com>
2023-11-04 13:19:43 +0000

    Prepare the panel footer for automatically showing the current count and total.

--- packages/qmi/src/panel-footer.tsx
+++ packages/qmi/src/panel-footer.tsx
@@ -4,22 +4,34 @@ import * as React from 'react';
 interface iPanelFooterProps {
 	cols: number;
 	count: number;
+	total?: number;
 	children?: React.ReactNode;
 }
 
 import {
 	__,
+	sprintf,
 } from '@wordpress/i18n';
 
 declare const QM_i18n: iQM_i18n;
 
-export const PanelFooter = ( { children, cols, count }: iPanelFooterProps ) => (
+export const PanelFooter = ( { children, cols, count, total = count }: iPanelFooterProps ) => (
 	<tfoot>
 		<tr>
 			<td colSpan={ cols }>
 				{ __( 'Total:', 'query-monitor' ) }
 				&nbsp;
-				<span className="qm-items-number">{ QM_i18n.number_format( count ) }</span>
+				<span className="qm-items-number">
+					{ ( total === count ) ? (
+						QM_i18n.number_format( count )
+					) : (
+						sprintf(
+							'%1$s / %2$s',
+							QM_i18n.number_format( count ),
+							QM_i18n.number_format( total )
+						)
+					) }
+				</span>
 			</td>
 			{ children }
 		</tr>
