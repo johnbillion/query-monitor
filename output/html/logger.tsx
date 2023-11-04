6ba7ffd0177bd69005708580fbf0a126b066f984
John Blackbourn <johnbillion@gmail.com>
2023-11-04 13:13:40 +0000

    Move the Logs panel over to the declarative table.

--- output/html/logger.tsx
+++ output/html/logger.tsx
@@ -1,11 +1,8 @@
-import * as classNames from 'classnames';
 import {
-	Caller,
 	iPanelProps,
-	Component,
-	Tabular,
+	TabularPanel,
 	Warning,
-	Notice,
+	EmptyPanel,
 } from 'qmi';
 import {
 	DataTypes,
@@ -14,10 +11,10 @@ import * as React from 'react';
 
 import { __ } from '@wordpress/i18n';
 
-export default ( { data, id }: iPanelProps<DataTypes['Logger']> ) => {
-	if ( ! data.logs || ! data.logs.length ) {
+export default ( { data }: iPanelProps<DataTypes['Logger']> ) => {
+	if ( ! data.logs.length ) {
 		return (
-			<Notice id={ id }>
+			<EmptyPanel>
 				<p>
 					{ __( 'No data logged.', 'query-monitor' ) }
 				</p>
@@ -26,50 +23,33 @@ export default ( { data, id }: iPanelProps<DataTypes['Logger']> ) => {
 						{ __( 'Read about profiling and logging in Query Monitor.', 'query-monitor' ) }
 					</a>
 				</p>
-			</Notice>
+			</EmptyPanel>
 		);
 	}
 
-	return (
-		<Tabular id={ id }>
-			<thead>
-				<tr>
-					<th scope="col">
-						{ __( 'Level', 'query-monitor' ) }
-					</th>
-					<th scope="col">
-						{ __( 'Message', 'query-monitor' ) }
-					</th>
-					<th scope="col">
-						{ __( 'Caller', 'query-monitor' ) }
-					</th>
-					<th scope="col">
-						{ __( 'Component', 'query-monitor' ) }
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-				{ data.logs.map( ( row ) => {
-					const warn = data.warning_levels.includes( row.level );
-					const classes = {
-						'qm-warn': warn,
-					};
-
-					return (
-						<tr key={ `${ row.level }${ row.message }` } className={ classNames( classes ) }>
-							<td>
-								{ warn && ( <Warning /> ) }
-								{ row.level }
-							</td>
-							<td>
-								{ row.message }
-							</td>
-							<Caller trace={ row.trace } />
-							<Component component={ row.trace.component } />
-						</tr>
-					);
-				} ) }
-			</tbody>
-		</Tabular>
-	);
+	return <TabularPanel
+		title={ __( 'Logs', 'query-monitor' ) }
+		cols={ {
+			level: {
+				heading: __( 'Level', 'query-monitor' ),
+				render: ( row ) => (
+					<>
+						{ data.warning_levels.includes( row.level ) && ( <Warning /> ) }
+						{ row.level }
+					</>
+				),
+			},
+			message: {
+				heading: __( 'Message', 'query-monitor' ),
+				render: ( row ) => row.message,
+			},
+			caller: {
+				heading: __( 'Caller', 'query-monitor' ),
+			},
+			component: {
+				heading: __( 'Component', 'query-monitor' ),
+			},
+		} }
+		data={ data.logs }
+	/>
 };
