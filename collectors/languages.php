10eedafd54f858d74f3165d2cffd35770a116633
John Blackbourn <johnbillion@gmail.com>
2024-07-25 10:49:49 +0100

    Remove back-compat code for pre WordPress 5.0.

--- collectors/languages.php
+++ collectors/languages.php
@@ -52,8 +52,7 @@ class QM_Collector_Languages extends QM_DataCollector {
 	public function collect_locale_data() {
 		$this->data->locale = get_locale();
 		$this->data->user_locale = get_user_locale();
-		// WP 5.0
-		$this->data->determined_locale = function_exists( 'determine_locale' ) ? determine_locale() : get_locale();
+		$this->data->determined_locale = determine_locale();
 		$this->data->language_attributes = get_language_attributes();
 
 		if ( function_exists( '\Inpsyde\MultilingualPress\siteLanguageTag' ) ) {
