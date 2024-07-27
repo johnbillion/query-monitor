2c2c40c8a42f00d7bf27e6c125fbd9c61f805559
John Blackbourn <johnbillion@gmail.com>
2024-07-27 23:47:03 +0100

    This function exists since 5.6.1.

--- collectors/environment.php
+++ collectors/environment.php
@@ -224,11 +224,8 @@ class QM_Collector_Environment extends QM_DataCollector {
 			'host' => null,
 			'OS' => null,
 			'arch' => null,
+			'basicauth' => wp_is_site_protected_by_basic_auth() ? 'true' : 'false',
 		);
-
-		if ( function_exists( 'wp_is_site_protected_by_basic_auth' ) ) {
-			$this->data->server['basicauth'] = wp_is_site_protected_by_basic_auth() ? 'true' : 'false';
-		}
 
 		if ( function_exists( 'php_uname' ) ) {
 			$this->data->server['host'] = php_uname( 'n' );
