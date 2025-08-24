6417db119c74c4f64cba197703fcdc1b88073970
John Blackbourn <john@johnblackbourn.com>
2025-08-24 22:33:50 +0100

    Adjust this further so we don't get an undefined index warning.

--- collectors/raw_request.php
+++ collectors/raw_request.php
@@ -69,12 +69,11 @@ class QM_Collector_Raw_Request extends QM_DataCollector {
 		$headers = array();
 		$raw_headers = headers_list();
 		foreach ( $raw_headers as $row ) {
-			list( $key, $value ) = explode( ':', $row, 2 );
+			$parts = explode( ':', $row, 2 );
+			$key = trim( $parts[0] );
 			// This is needed when a header is sent without a value, for example: header('X-Hello');
-			// See https://github.com/phpstan/phpstan/issues/10854
-			if ( null !== $value ) {
-				$headers[ trim( $key ) ] = trim( $value );
-			}
+			$value = isset( $parts[1] ) ? trim( $parts[1] ) : '';
+			$headers[ $key ] = $value;
 		}
 
 		ksort( $headers );
