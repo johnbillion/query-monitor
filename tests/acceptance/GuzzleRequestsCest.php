710e6c9523cb837936ed08103c9279f26bd008ce
John Blackbourn <john@johnblackbourn.com>
2025-08-26 20:03:33 +0100

    Mock the response to the guzzle request so we don't hit httpbin during testing.

--- tests/acceptance/GuzzleRequestsCest.php
+++ tests/acceptance/GuzzleRequestsCest.php
@@ -11,12 +11,12 @@ class GuzzleRequestsCest {
 	public function SuccessfulGuzzleRequestShouldBeLogged( AcceptanceTester $I ): void {
 		$I->amOnAPageThatMakesGuzzleRequest( 'successful_request' );
 		$I->seeQMMenu();
-		$I->seeInQMPanel( 'HTTP API Calls (1)', 'https://httpbin.org/json' );
+		$I->seeInQMPanel( 'HTTP API Calls (1)', 'https://example.org/json' );
 	}
 
 	public function ErrorGuzzleRequestShouldBeLogged( AcceptanceTester $I ): void {
 		$I->amOnAPageThatMakesGuzzleRequest( 'error_request' );
 		$I->seeQMMenuWithWarning();
-		$I->seeInQMPanel( 'HTTP API Calls (1)', 'https://httpbin.org/status/404' );
+		$I->seeInQMPanel( 'HTTP API Calls (1)', 'https://example.org/status/404' );
 	}
 }
