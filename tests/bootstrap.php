4f6e1ec9745059075e5c46334f770995ed37447d
John Blackbourn <john@johnblackbourn.com>
2026-01-27 15:53:32 +0100

    Register the real themes directory and switch to a real theme after WordPress is loaded.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,5 +11,11 @@ require_once $_tests_dir . 'includes/functions.php';
 // Manually load the plugin
 tests_add_filter( 'muplugins_loaded', fn() => require_once dirname( __DIR__ ) . '/query-monitor.php' );
 
+// Register the real themes directory and switch to a real theme after WordPress is loaded
+tests_add_filter( 'setup_theme', function() {
+	register_theme_directory( '/var/www/html/wp-content/themes' );
+	switch_theme( 'twentytwentyfive' );
+} );
+
 // Start up the WP testing environment
 require_once $_tests_dir . 'includes/bootstrap.php';
