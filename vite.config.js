9ab120ec8ec441f64d84578be47f54211bab3e6c
John Blackbourn <john@johnblackbourn.com>
2026-02-04 17:27:23 +0000

    More i18n tweaks.

--- vite.config.js
+++ vite.config.js
@@ -26,10 +26,18 @@ export default defineConfig( {
 	build: {
 		sourcemap: false,
 		target: 'chrome112',
+		// Terser is used instead of esbuild for minification in order to preserve
+		// wp i18n function names so translate.wordpress.org can extract them.
 		minify: 'terser',
 		terserOptions: {
 			mangle: {
+				// Prevent i18n function names from being shortened.
 				reserved: [ '__', '_x', '_n', '_nx', 'sprintf' ],
+			},
+			compress: {
+				// Prevent ternary expressions from being moved inside i18n
+				// function arguments, which makes them unextractable.
+				conditionals: false,
 			},
 		},
 		rollupOptions: {
