f3bf57db72ab87788233343786a2a2133d12c930
John Blackbourn <john@johnblackbourn.com>
2026-02-10 23:44:02 +0000

    Version 4.0.0-alpha.2.

--- query-monitor.php
+++ query-monitor.php
@@ -10,7 +10,7 @@
  *
  * Plugin Name:  Query Monitor
  * Description:  The developer tools panel for WordPress.
- * Version:      4.0.0-alpha.1
+ * Version:      4.0.0-alpha.2
  * Plugin URI:   https://querymonitor.com/
  * Plugin ID:    did:plc:q2afge25l63iz553aumeqi3w
  * Author:       John Blackbourn
@@ -37,7 +37,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'QM_VERSION', '4.0.0-alpha.1' );
+define( 'QM_VERSION', '4.0.0-alpha.2' );
 
 // This must be required before vendor/autoload.php so QM can serve its own message about PHP compatibility.
 require_once __DIR__ . '/classes/PHP.php';
