d4fe453f75e681af7c3612f9e69a211936954156
John Blackbourn <john@johnblackbourn.com>
2026-03-18 00:06:49 +0000

    Reinstate highlighting in tables that use grouped rows (ie. the Hooks panel table).

--- output/table.tsx
+++ output/table.tsx
@@ -251,6 +251,8 @@ export const Table = <TDataRow extends {}, TCols extends Cols<TDataRow> = Cols<T
 	const total = countData( data );
 	const nonEmptyCols = Object.entries( cols ).filter( ( entry ): entry is [string, Col<TDataRow>] => ( entry[1] ? true : false ) );
 
+	const highlightedRows = new Set<number>();
+
 	for ( const [ filterName, filterValue ] of Object.entries( filters ) ) {
 		if ( ! ( filterName in cols ) ) {
 			continue;
@@ -276,6 +278,13 @@ export const Table = <TDataRow extends {}, TCols extends Cols<TDataRow> = Cols<T
 				}
 			}
 			data = data.filter( ( row ) => matchingGroups.has( groupKey( row ) ) );
+
+			// Highlight individual rows within a group that match the filter
+			for ( let i = 0; i < data.length; i++ ) {
+				if ( colFilters.callback( data[ i ], filterValue ) ) {
+					highlightedRows.add( i );
+				}
+			}
 		} else {
 			data = data.filter( ( row ) => colFilters.callback( row, filterValue ) );
 		}
@@ -374,6 +383,7 @@ export const Table = <TDataRow extends {}, TCols extends Cols<TDataRow> = Cols<T
 						key={ i }
 						className={ clsx( {
 							'qm-warn': rowHasError && rowHasError( row ),
+							'qm-highlight': highlightedRows.has( i ),
 						} ) }
 					>
 						{ nonEmptyCols.map( ( [ key, col ] ) => {
