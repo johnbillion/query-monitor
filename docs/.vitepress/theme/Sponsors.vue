30422a46f75bd76881ac7e91a602dd7ea58641c7
John Blackbourn <john@johnblackbourn.com>
2026-06-24 12:53:29 +0100

    Docs.

---
+++ docs/.vitepress/theme/Sponsors.vue
@@ -0,0 +1,35 @@
+<template>
+	<p>Without the support of sponsors, I would certainly not be able to continue maintaining Query Monitor.</p>
+
+	<p>If you work at an agency or web host that develops with WordPress, ask your company to provide <a href="https://github.com/sponsors/johnbillion">sponsorship in order to invest in its supply chain</a>. The tools that I maintain probably save your company time and money. GitHub sponsorship can be done at the organisation level.</p>
+
+	<p>Many thanks to the following companies and individuals that sponsor the time that I spend maintaining this plugin:</p>
+
+	<p align="center">
+		<a href="https://automattic.com">
+			<img src="https://cdn.jsdelivr.net/gh/johnbillion/johnbillion@latest/assets/sponsors/automattic.svg" alt="Automattic" style="max-height:40px">
+		</a>
+	</p>
+
+	<p align="center">
+		<a href="https://servmask.com">
+			<img src="https://cdn.jsdelivr.net/gh/johnbillion/johnbillion@latest/assets/sponsors/servmask.svg" alt="ServMask" style="max-height:40px">
+		</a>
+	</p>
+
+	<p align="center">
+		<a href="https://wp-staging.com">
+			<img src="https://cdn.jsdelivr.net/gh/johnbillion/johnbillion@latest/assets/sponsors/wp-staging.png" alt="WP Staging" style="max-height:40px">
+		</a>
+	</p>
+
+	<p>Plus all my kind sponsors on GitHub:</p>
+
+	<p align="center">
+		<a href="https://github.com/sponsors/johnbillion">
+			<img src="https://cdn.jsdelivr.net/gh/johnbillion/johnbillion@latest/sponsors.svg" alt="Sponsors" />
+		</a>
+	</p>
+
+	<p><a href="https://github.com/sponsors/johnbillion">Click here to find out about supporting the open source tools and plugins that I maintain</a>.</p>
+</template>
