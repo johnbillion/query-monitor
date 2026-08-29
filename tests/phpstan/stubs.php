6895e6992ee4e8a00573ffda19948a1a4baf4ccd
John Blackbourn <john@johnblackbourn.com>
2026-08-29 22:05:52 +0200

    Use our own local stubs for WP-CLI.

--- tests/phpstan/stubs.php
+++ tests/phpstan/stubs.php
@@ -15,3 +15,32 @@ define( 'Altis\ROOT_DIR', '' );
 function members_register_cap_group( string $name, array $args = [] ): void {}
 
 function members_register_cap( string $name, array $args = [] ): void {}
+
+// Third party classes:
+
+class WP_CLI {
+
+	/**
+	 * @param string $name
+	 * @param callable|object|string $callable
+	 * @param array<string, mixed> $args
+	 * @return bool
+	 */
+	public static function add_command( string $name, $callable, array $args = [] ): bool {
+		return true;
+	}
+
+	public static function success( string $message ): void {}
+
+	/**
+	 * @param string|WP_Error|Exception|Throwable $message
+	 */
+	public static function warning( $message ): void {}
+
+	/**
+	 * @param string|WP_Error|Exception|Throwable $message
+	 * @param bool|int $exit
+	 */
+	public static function error( $message, $exit = true ): void {}
+
+}
